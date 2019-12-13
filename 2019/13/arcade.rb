355619206a748bbce5fd862f82b93b92c642d24c
Joakim Tufvegren <jocke@barbanet.com>
2019-12-13 12:12:58 +0100

    Add optional graphics mode to 2019/13.
    Run with --graphics to enable.

--- 2019/13/arcade.rb
+++ 2019/13/arcade.rb
@@ -3,6 +3,8 @@ input = File.read('input').strip
 require 'set'
 require_relative '../lib/intcode'
 
+@graphics = false
+
 @game = Intcode.new(input, false)
 @grid = []
 @blocks = Set.new
@@ -16,42 +18,38 @@ def handle_output
 
     if x == -1 and y == 0
       @score = @game.output
+      if @graphics
+        Curses.setpos(0, 0)
+        Curses.addstr("Score: #{@score}")
+        Curses.refresh
+      end
     else
       tile = @game.output
+      tile_ch = ' '
       case tile
       when 0
         @blocks.delete? [x,y]
+      when 1
+        tile_ch = "\u2588"
       when 2
         @blocks << [x,y]
+        tile_ch = "\u2592"
       when 3
         @paddle = x
+        tile_ch = "\u2594"
       when 4
         @ball = x
+        tile_ch = "\u2022"
       end
       @grid[y] ||= []
       @grid[y][x] = tile
+      if @graphics
+        Curses.setpos(y + 1, x)
+        Curses.addstr(tile_ch)
+        Curses.refresh
+      end
     end
   end
- # Uncomment for (bad) graphics.
-=begin
-  @grid.each do |line|
-    line.each do |tile|
-      print case tile
-            when 0
-              ' '
-            when 1
-              "\u2588"
-            when 2
-              '#'
-            when 3
-              '='
-            when 4
-              'o'
-            end
-    end
-    puts
-  end
-=end
 end
 
 # part 1
@@ -61,17 +59,38 @@ handle_output
 puts "#{@blocks.count} blocks found on screen"
 
 # part 2
-@game.reset
-@game[0] = 2
-@score = 0
-@game.run do
-  handle_output
-
-  # Joystick input
-  @ball <=> @paddle
+if ARGV.include?('--graphics')
+  begin
+    require 'curses'
+    @graphics = true
+  rescue LoadError
+    puts 'Failed to load Curses, running without graphics'
+  end
 end
 
-handle_output
+begin
+  if @graphics
+    Curses.init_screen
+    Curses.curs_set(0)
+  end
+  @game.reset
+  @game[0] = 2
+  @score = 0
+  @game.run do
+    handle_output
+    if @graphics
+      sleep 0.01
+    end
+
+    # Joystick input
+    @ball <=> @paddle
+  end
+
+  handle_output
+ensure
+  Curses.close_screen if @graphics
+end
+
 if not @blocks.empty?
   raise "Game Over?"
 end
