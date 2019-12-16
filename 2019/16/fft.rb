705c048be5f368e7766826b985282f9f77d1de27
Joakim Tufvegren <jocke@barbanet.com>
2019-12-16 12:05:22 +0100

    2019/16 completed.
    Part 2 solved both with and without Numo/NArray.
    With Numo/NArray is about 10-20 times faster than without, depending on
    hardware.

--- 2019/16/fft.rb
+++ 2019/16/fft.rb
@@ -2,6 +2,9 @@ input = File.read('input').strip
 #input = '80871224585914546619083218645595'
 #input = '19617804207202209144916044189917'
 #input = '69317163492948606335995924319873'
+#input = '03036732577212944063491565474664'
+#input = '02935109699940807407585447034323'
+#input = '03081770884921959731165446850517'
 
 @list = input.chars.map(&:to_i)
 
@@ -39,3 +42,41 @@ list = @list
   list = fft(list)
 end
 puts "First eight digits: #{list[0...8].join}"
+
+
+# part 2
+offset = @list[0...7].join.to_i
+raise "Offset has to end up in latter half, this won't work. :(" if offset < @list.length * 5000
+
+begin
+  require 'numo/narray'
+
+  puts 'Running with Numo/NArray :)'
+
+  list = Numo::UInt32[*@list].tile(10000)[offset..-1].reverse
+
+  100.times do |i|
+    list = list.cumsum % 10
+  end
+
+  message = list.reverse[0...8].to_a.join
+
+rescue LoadError
+  puts 'Running without Numo/NArray :('
+
+  @real_list = @list * 10000
+  list = @real_list[offset..-1].reverse
+  100.times do |i|
+    sum = 0
+    new_list = []
+    list.each do |digit|
+      sum += digit
+      new_list << sum.abs % 10
+    end
+    list = new_list
+  end
+
+  message = list.reverse[0...8].join
+end
+
+puts "Message: #{message}"
