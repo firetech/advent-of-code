95fc348bf65aef6a7ab110cf2cfd83ad9256a481
Joakim Tufvegren <jocke@barbanet.com>
2019-12-22 12:37:24 +0100

    2019/22 completed.
    Had to get a lot of external "inspiration" for this, as part 2 was way
    over my head.

--- 2019/22/shuffle.rb
+++ 2019/22/shuffle.rb
@@ -1,29 +1,138 @@
-input = File.read('input'); CARDS = 10007
-#input = File.read('example'); CARDS = 10 
+input = File.read('input')
 
-# part 1
-@deck = (0...CARDS).to_a
+@steps = []
 input.strip.split("\n").each do |line|
   case line
   when 'deal into new stack'
-    @deck.reverse!
+    @steps << [:rev]
   when /\Acut (-?\d+)\z/
-    cut = Regexp.last_match[1].to_i
-    @deck = @deck[cut..-1] + @deck[0...cut]
+    @steps << [:cut, Regexp.last_match[1].to_i]
   when /\Adeal with increment (\d+)\z/
-    step = Regexp.last_match[1].to_i
-    new_deck = Array.new(@deck.length)
-    i = 0
-    until @deck.empty?
-      new_deck[i] = @deck.shift
-      unless @deck.empty?
-        until new_deck[i].nil?
-          i = (i + step) % new_deck.length
-        end
-      end
-    end
-    @deck = new_deck
+    @steps << [:inc, Regexp.last_match[1].to_i]
+  else
+    raise "Malformed line: #{line}"
   end
 end
 
-puts "Position of card 2019: #{@deck.index(2019)}"
+# Graciously stolen from Rosetta Code (https://rosettacode.org/wiki/Modular_inverse#Ruby)
+def modinv(a, m) # compute a^-1 mod m if possible
+  raise "NO INVERSE - #{a} and #{m} not coprime" unless a.gcd(m) == 1
+  return m if m == 1
+  m0, inv, x0 = m, 1, 0
+  while a > 1
+    inv -= (a / m) * x0
+    a, m = m, a % m
+    inv, x0 = x0, inv
+  end
+  inv += m0 if inv < 0
+  return inv
+end
+
+def apply(steps, deck)
+  cards = deck.length
+  steps.each do |op, arg|
+    case op
+    when :rev
+      deck = deck.reverse
+    when :cut
+      deck = deck.rotate(arg)
+    when :inc
+      inv = modinv(arg, cards)
+      deck = deck.each_index.map { |i| deck[(i * inv) % cards] }
+    end
+  end
+  return deck
+end
+
+# part 1
+after_shuffle = apply(@steps, (0...10007).to_a)
+puts "Position of card 2019: #{after_shuffle.index(2019)}"
+
+
+# part 2
+def simplify_step(steps, i, cards)
+  return if i >= steps.length - 1
+  op1, arg1 = steps[i]
+  op2, arg2 = steps[i + 1]
+  case [op1, op2]
+  when [:rev, :rev]
+    # merge by eliminating
+    steps[i, 2] = []
+  when [:cut, :cut]
+    # merge by adding args
+    steps[i, 2] = [
+      [:cut, (arg1 + arg2) % cards]
+    ]
+  when [:inc, :inc]
+    # merge by multiplying args
+    steps[i, 2] = [
+      [:inc, (arg1 * arg2) % cards]
+    ]
+  when [:rev, :cut]
+    # invert order to move pairs of the same operation together
+    steps[i, 2] = [
+      [:cut, -arg2],
+      steps[i]
+    ]
+  when [:rev, :inc]
+    # invert order to move pairs of the same operation together
+    # needs an additional :cut, though
+    steps[i, 2] = [
+      steps[i + 1],
+      [:cut, -arg2 + 1],
+      steps[i]
+    ]
+  when [:cut, :inc]
+    # invert order to move pairs of the same operation together
+    steps[i, 2] = [
+      steps[i + 1],
+      [:cut, (arg1 * arg2) % cards]
+    ]
+  end
+end
+
+def simplify(steps, cards)
+  last = nil
+  steps = steps.dup
+  until steps == last
+    last = steps.dup
+    steps.each_index { |i| simplify_step(steps, i, cards) }
+  end
+  return steps
+end
+
+after_simplified_shuffle = apply(simplify(@steps, 10007), (0...10007).to_a)
+if after_simplified_shuffle != after_shuffle
+  raise "Simplification doesn't produce identical results"
+end
+
+
+cards = 119315717514047
+steps = simplify(@steps, cards)
+
+n = 101741582076661
+pow = 1
+bits = {}
+while pow < n
+  bits[pow] = steps
+  pow <<= 1
+  steps = simplify(steps + steps, cards)
+end
+
+bits_of_n = bits.keys.select { |b| n & b == b }
+if bits_of_n.inject(0) { |sum, x| sum + x } != n
+  raise "Sum of bits is not the same as value"
+end
+
+pos = 2020
+simplify(bits_of_n.flat_map { |b| bits[b] }, cards).reverse_each do |op, arg|
+  case op
+  when :rev
+    pos = cards - 1 - pos
+  when :cut
+    pos = (pos + arg) % cards
+  when :inc
+    pos = (pos * modinv(arg, cards)) % cards
+  end
+end
+puts "Position of card 2020: #{pos}"
