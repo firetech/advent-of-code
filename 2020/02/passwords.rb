a7c1c4248941cdb9fde502f3d195d7e00d3e25dd
Joakim Tufvegren <jocke@barbanet.com>
2020-12-03 10:17:42 +0100

    2020/02 completed.
    (Forgot to commit this yesterday.)

---
+++ 2020/02/passwords.rb
@@ -0,0 +1,31 @@
+input = File.read('input').strip.split("\n")
+#input = File.read('example1').strip.split("\n")
+
+#part 1
+valid = 0
+input.each do |line|
+  if line =~ /\A(\d+)-(\d+) (\w): (\w+)\z/
+    _, min, max, char, password = Regexp.last_match.to_a
+    if (min.to_i..max.to_i).include?(password.count(char))
+      valid += 1
+    end
+  else
+    raise "Invalid line: '#{line}'"
+  end
+end
+puts "Part 1: #{valid} passwords are valid"
+
+
+#part 2
+valid = 0
+input.each do |line|
+  if line =~ /\A(\d+)-(\d+) (\w): (\w+)\z/
+    _, pos1, pos2, char, password = Regexp.last_match.to_a
+    if (password[pos1.to_i - 1] == char) ^ (password[pos2.to_i - 1] == char)
+      valid += 1
+    end
+  else
+    raise "Invalid line: '#{line}'"
+  end
+end
+puts "Part 2: #{valid} passwords are valid"
