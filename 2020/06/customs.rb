9aada9de58775ee138e2dce7ca74cbd1665490f2
Joakim Tufvegren <jocke@barbanet.com>
2020-12-06 11:23:00 +0100

    2020/06 completed.

---
+++ 2020/06/customs.rb
@@ -0,0 +1,21 @@
+require 'set'
+
+input = File.read('input').strip
+#input = File.read('example1').strip
+
+groups = input.split("\n\n").map { |group| group.split("\n").map(&:chars) }
+
+count_any = 0 #part 1
+count_all = 0 #part 2
+groups.each do |group|
+  questions_any = Set.new #part 1
+  questions_all = Set.new('a'..'z') #part 2
+  group.each do |answers|
+    questions_any += answers #part 1
+    questions_all &= answers #part 2
+  end
+  count_any += questions_any.length #part 1
+  count_all += questions_all.length #part 2
+end
+puts "Sum of any-yes questions: #{count_any}" #part 1
+puts "Sum of all-yes questions: #{count_all}" #part 2
