dd5ca1602d7080590c30ccac68c9019ae58df367
Joakim Tufvegren <jocke@barbanet.com>
2020-12-09 12:08:49 +0100

    2020/09 completed.

---
+++ 2020/09/xmas.rb
@@ -0,0 +1,36 @@
+input = File.read('input').strip.split("\n").map(&:to_i); preamble_size = 25
+#input = File.read('example1').strip.split("\n").map(&:to_i); preamble_size = 5
+
+#part 1
+prev_buf = []
+waiting_buf = input.clone
+preamble_size.times { prev_buf << waiting_buf.shift }
+found = true
+target = nil
+while found
+  found = false
+  target = waiting_buf.shift
+  prev_buf.combination(2) do |a,b|
+    if a + b == target
+      found = true
+      break
+    end
+  end
+  prev_buf.shift
+  prev_buf << target
+end
+puts "First non-matching number: #{target}"
+
+#part 2
+input.each_with_index do |val, i|
+  first_i = i
+  while val < target
+    i += 1
+    val += input[i]
+  end
+  if val == target
+    numbers = input[first_i..i]
+    puts "Encryption weakness: #{numbers.min + numbers.max}"
+    break
+  end
+end
