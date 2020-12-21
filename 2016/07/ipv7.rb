aee3f4301f38e57abff0d7547fdc9e1173bd4591
Joakim Tufvegren <jocke@barbanet.com>
2020-12-21 21:36:31 +0100

    2016/07 completed.
    Complex regex hurts my brain, and I initially had some error in my input
    file, causing me to miss 2 IPs in part 2. Yay.

---
+++ 2016/07/ipv7.rb
@@ -0,0 +1,22 @@
+file = 'input'
+#file = 'example1'
+#file = 'example2'
+
+input = File.read(file).strip.split("\n")
+
+# Part 1
+abba = '([[:lower:]])((?!\1)[[:lower:]])\2\1'
+supports_tls = input.count do |ip|
+  ip =~ /#{abba}/ and not ip =~ /\[[^\]]*#{abba}[^\]]*\]/
+end
+puts "#{supports_tls} IPs support TLS"
+
+# Part 2
+first = '([[:lower:]])((?!\1)[[:lower:]])\1'
+second = '\2\1\2'
+supports_ssl = input.count do |ip|
+  ip =~ /(?:\A|\])(?:[^\[]*)#{first}.*\[[^\]]*#{second}[^\]]*\]/ or
+    ip =~ /\[[^\]]*#{first}(?:[^\]]*?\][^\[]*?\[)*[^\]]*?\][^\[]*?#{second}/
+end
+puts "#{supports_ssl} IPs support SSL"
+
