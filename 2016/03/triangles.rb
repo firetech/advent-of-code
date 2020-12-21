264d7f9fe8b62667238bbdf131fbbc5a3f0c2cc8
Joakim Tufvegren <jocke@barbanet.com>
2020-12-21 19:09:59 +0100

    2016/03 completed.

---
+++ 2016/03/triangles.rb
@@ -0,0 +1,16 @@
+input = File.read('input').strip.split("\n").map { |line| line.strip.split(/\s+/).map(&:to_i) }
+
+def possible_triangles(input)
+  input.count do |lengths|
+    a, b, c = lengths.sort
+    a + b > c
+  end
+end
+
+# Part 1
+puts "#{possible_triangles(input)} possible triangles (row oriented)"
+
+# Part 2
+new_input = input.transpose.map { |col| col.each_slice(3).to_a }.flatten(1)
+puts "#{possible_triangles(new_input)} possible triangles (column oriented)"
+
