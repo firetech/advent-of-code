ae28712010a45ab9447057a9e0b397ad6ea1058c
Joakim Tufvegren <jocke@barbanet.com>
2020-12-21 20:13:08 +0100

    2016/06 completed.

---
+++ 2016/06/repcode.rb
@@ -0,0 +1,18 @@
+file = 'input'
+#file = 'example1'
+
+input = File.read(file).strip.split("\n").map(&:chars)
+counts = Array.new(input.first.length) { Hash.new(0) }
+input.each do |line|
+  line.each_with_index do |c, i|
+    counts[i][c] += 1
+  end
+end
+
+# Part 1
+code = counts.map { |list| list.max_by { |c, n| n }.first }.join
+puts "Code (most common): #{code}"
+
+# Part 2
+code = counts.map { |list| list.min_by { |c, n| n }.first }.join
+puts "Code (least common): #{code}"
