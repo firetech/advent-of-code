4f0264a12cdddf48234223555985793bbc32694c
Joakim Tufvegren <jocke@barbanet.com>
2020-12-26 21:56:39 +0100

    2016/13 completed.

---
+++ 2016/13/officemaze.rb
@@ -0,0 +1,33 @@
+@input = 1364; @target = [31, 39]
+#@input = 10; @target = [7, 4]
+
+def is_wall?(x, y)
+  n = x*x + 3*x + 2*x*y + y + y*y + @input
+  # Curiously enough, this seems to be the fastest method in Ruby to count set bits...
+  return (n.to_s(2).count('1') % 2 == 1)
+end
+
+queue = [[1, 1, 0]]
+distance = { [1, 1].hash => 0 }
+tkey = @target.hash
+while not queue.empty? and not distance.has_key?(tkey)
+  x, y, steps = queue.shift
+  [[0, -1], [0, 1], [-1, 0], [1, 0]].each do |dx, dy|
+    px, py = x + dx, y + dy
+    pkey = [px, py].hash
+    if px >= 0 and py >= 0 and not distance.has_key?(pkey)
+      if is_wall?(px, py)
+        distance[pkey] = nil
+      else
+        distance[pkey] = steps + 1
+        queue << [px, py, steps + 1]
+      end
+    end
+  end
+end
+
+# Part 1
+puts "Minimum steps to reach #{@target.join(',')}: #{distance[tkey]}"
+
+# Part 2
+puts "Number of locations reachable in 50 steps or less: #{distance.count{ |_, val| (not val.nil? and val <= 50) }}"
