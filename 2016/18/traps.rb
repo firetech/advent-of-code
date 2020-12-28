8d4ea300375616c3e0aa29a1968208fa1ba1f468
Joakim Tufvegren <jocke@barbanet.com>
2020-12-28 15:32:43 +0100

    2016/18 completed.

---
+++ 2016/18/traps.rb
@@ -0,0 +1,19 @@
+file = 'input'; rows = [40, 400000]
+#file = 'example1'; rows = [3]
+#file = 'example2'; rows = [10]
+
+input = File.read(file).strip.chars.map { |c| c == '^' }
+
+line = input
+row = 1
+safe_count = input.count(false)
+rows.each do |size|
+  while row < size
+    line = [false, *line, false].each_cons(3).map do |left, center, right|
+      ((left and not right) or (right and not left))
+    end
+    safe_count += line.count(false)
+    row += 1
+  end
+  puts "Safe tiles in #{size} rows: #{safe_count}"
+end
