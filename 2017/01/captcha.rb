eb490c215fe63910329fa418546eee477bbb4d29
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-04 15:08:55 +0100

    2017/01 completed.

---
+++ 2017/01/captcha.rb
@@ -0,0 +1,28 @@
+input = File.read('input').strip
+#input = '1122'
+#input = '1111'
+#input = '1234'
+#input = '91212129'
+#input = '1212'
+#input = '1221'
+#input = '123425'
+#input = '123123'
+#input = '12131514'
+
+chars = input.chars
+
+sum = {
+  1 => 0,
+  chars.length/2 => 0
+}
+chars.each_with_index do |c, i|
+  sum.each_key do |offset|
+    if c == chars[(i + offset) % chars.length]
+      sum[offset] += c.to_i
+    end
+  end
+end
+
+sum.each do |offset, value|
+  puts "CAPTCHA answer with offset #{offset}: #{value}"
+end
