6bd389bad5d7728c03cfaab26f06240d01fced43
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-05 11:49:23 +0100

    2017/04 completed.

---
+++ 2017/04/passphrase.rb
@@ -0,0 +1,27 @@
+input = File.read('input').strip
+#input = File.read('example1').strip
+#input = File.read('example2').strip
+
+lines = input.split("\n")
+valid1 = 0
+valid2 = 0
+lines.each do |line|
+  words = line.split(/\s+/)
+  if words.uniq.length == words.length
+    valid1 += 1
+    anagrams = []
+    words.each do |word|
+      chars = word.chars.sort
+      if anagrams.include?(chars)
+        break
+      end
+      anagrams << chars
+    end
+    if anagrams.length == words.length
+      valid2 += 1
+    end
+  end
+end
+
+puts "#{valid1} passwords without repeated words"
+puts "#{valid2} passwords without repeated anagrams"
