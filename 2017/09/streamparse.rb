ce2b348b5335221d6258e707005c00e20b6e8336
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-05 16:03:39 +0100

    2017/09 completed.

---
+++ 2017/09/streamparse.rb
@@ -0,0 +1,61 @@
+input = File.read('input').strip
+#input = '{}'
+#input = '{{{}}}'
+#input = '{{},{}}'
+#input = '{{{},{},{{}}}}'
+#input = '{<a>,<a>,<a>,<a>}'
+#input = '{{<ab>},{<ab>},{<ab>},{<ab>}}'
+#input = '{{<!!>},{<!!>},{<!!>},{<!!>}}'
+#input = '{{<a!>},{<a!>},{<a!>},{<ab>}}'
+
+def parse_group(stream, offset = 1, level = 1)
+  score = level
+  garbage = 0
+  while offset < stream.length
+    case stream[offset]
+    when '{'
+      grp_score, grp_garbage, offset = parse_group(stream, offset+1, level+1)
+      score += grp_score
+      garbage += grp_garbage
+    when '}'
+      return score, garbage, offset+1
+    when '<'
+      offset, size = parse_garbage(stream, offset+1)
+      garbage += size
+    when ','
+      offset += 1
+    else
+      raise "Unexpected character in group: '#{stream[offset]}' (@#{offset})"
+    end
+  end
+  raise "Stream ended mid-group"
+end
+
+def parse_garbage(stream, offset = 1)
+  count = 0
+  while offset < stream.length
+    case stream[offset]
+    when '!'
+      offset += 1
+    when '>'
+      return offset+1, count
+    else
+      count += 1
+    end
+    offset += 1
+  end
+  raise "Stream ended mid-garbage"
+end
+
+stream = input.chars
+if stream.first != '{'
+  raise "Expected '{' at start of input"
+end
+
+score, garbage, offset = parse_group(stream, 1)
+if offset < stream.length
+  raise "Outer group ended before stream"
+end
+
+puts "Total score: #{score}"
+puts "Garbage characters: #{garbage}"
