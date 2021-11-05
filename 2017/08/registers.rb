692907b33df3e1098af441c582b53f863ed2f9c3
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-05 14:20:10 +0100

    2017/08 completed.

---
+++ 2017/08/registers.rb
@@ -0,0 +1,27 @@
+input = File.read('input').strip
+#input = File.read('example1').strip
+
+OPS = {
+  'inc' => :+,
+  'dec' => :-
+}
+
+regs = Hash.new(0)
+seen_max = nil
+
+input.split("\n").each do |line|
+  if line =~ /\A(\w+) (inc|dec) (-?\d+) if (\w+) (<|>|<=|>=|==|!=) (-?\d+)\z/
+    _, reg, op, val, cond_reg, cond_op, cond_val = Regexp.last_match.to_a
+    if regs[cond_reg].send(cond_op.to_sym, cond_val.to_i)
+      regs[reg] = regs[reg].send(OPS[op], val.to_i)
+      if seen_max.nil? or seen_max < regs[reg]
+        seen_max = regs[reg]
+      end
+    end
+  else
+    raise "Malformed line: '#{line}'"
+  end
+end
+
+puts "Max at end of program: #{regs.values.max}"
+puts "Seen maximum: #{seen_max}"
