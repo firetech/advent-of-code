2fd33e8b08915b31a1762071e1a871f5b869146c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-05 12:43:24 +0100

    2017/06 completed.

---
+++ 2017/06/realloc.rb
@@ -0,0 +1,32 @@
+input = File.read('input').strip
+#input = '0 2 7 0'
+
+banks = input.split(/\s+/).map(&:to_i)
+
+seen = {}
+cycles = 0
+
+while not seen.include?(banks)
+  seen[banks.clone] = cycles
+
+  realloc = 0
+  index = 0
+  banks.each_with_index do |bank, i|
+    if bank > realloc
+      realloc = bank
+      index = i
+    end
+  end
+
+  banks[index] = 0
+  while realloc > 0
+    index = (index + 1) % banks.length
+    banks[index] += 1
+    realloc -= 1
+  end
+
+  cycles += 1
+end
+
+puts "Cycles until seen state: #{cycles}"
+puts "Loop size: #{cycles - seen[banks]}"
