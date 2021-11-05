720a0055ed277fb43ff2848dc0186c196f38e571
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-05 12:04:59 +0100

    2017/05 completed.

---
+++ 2017/05/jmp.rb
@@ -0,0 +1,30 @@
+input = File.read('input').strip
+#input = File.read('example1').strip
+
+program = input.split("\n").map(&:to_i)
+
+def run_program!(program)
+  pc = 0
+  steps = 0
+
+  while (0...program.length).include?(pc)
+    new_pc = pc + program[pc]
+    program[pc] = yield program[pc]
+    pc = new_pc
+    steps +=1
+  end
+
+  return steps
+end
+
+part1_steps = run_program!(program.clone) { |val| val + 1 }
+puts "Steps in always increasing mode: #{part1_steps}"
+
+part2_steps = run_program!(program.clone) do |val|
+  if val >= 3
+    val - 1
+  else
+    val + 1
+  end
+end
+puts "Steps in converging mode: #{part2_steps}"
