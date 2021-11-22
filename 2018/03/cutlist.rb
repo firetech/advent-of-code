2e704efd0d34f7ddd038843d207e4df87fce0a27
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-22 16:30:17 +0100

    2018/03 completed.

---
+++ 2018/03/cutlist.rb
@@ -0,0 +1,42 @@
+require 'set'
+
+file = 'input'
+#file = 'example1'
+
+@cuts = {}
+File.read(file).strip.split("\n").each do |line|
+  if line =~ /\A#(\d+) @ (\d+),(\d+): (\d+)x(\d+)\z/
+    _, id, x, y, w, h = Regexp.last_match.to_a.map(&:to_i)
+    @cuts[id] = [ x, y, w, h ]
+  else
+    raise "Malformed line: '#{line}'"
+  end
+end
+
+claims = {}
+collisions = Set[]
+good_claims = Set[]
+@cuts.each do |id, (x, y, w, h)|
+  good_claim = true
+  (x...x+w).each do |px|
+    (y...y+h).each do |py|
+      key = [px, py].hash
+      if claims.has_key?(key)
+        collisions << key
+        good_claims.delete(claims[key])
+        good_claim = false
+      else
+        claims[key] = id
+      end
+    end
+  end
+  if good_claim
+    good_claims << id
+  end
+end
+
+# Part 1
+puts "Squares with multiple claims: #{collisions.length}"
+
+# Part 2
+puts "Good claims: #{good_claims.map { |x| "##{x}" }.join(', ')}"
