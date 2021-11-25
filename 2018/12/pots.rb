bbe6c4d730f00cbce07558c1e1a399656a389e0b
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-25 19:07:22 +0100

    2018/12 improved.
    My initial idea was to use (and practice) bit masking for calculating
    the states, but after realizing that the used pots could go below 0, I
    dismissed that idea after implementing it halfway. Later, I realized I
    could keep track of the minimum ID separately and still use bit masks.
    This is the result, a bit faster than the previous version.

--- 2018/12/pots.rb
+++ 2018/12/pots.rb
@@ -1,72 +1,96 @@
-require 'set'
-
 file = 'input'
 #file = 'example1'
 
 GENERATIONS = [ 20, 50_000_000_000 ]
 
+def potstr_to_i(str)
+  val = 0
+  str.each_char.with_index do |c, i|
+    if c == '#'
+      val |= 1 << i
+    end
+  end
+  return val
+end
+
+def pots_sum(pots, min)
+  sum = 0
+  i = 0
+  while pots > 0
+    sum += i + min if pots & 1 != 0
+    i += 1
+    pots >>= 1
+  end
+  return sum
+end
+
 @transforms = Hash.new(false)
-@pots = Set[]
 File.read(file).strip.split("\n").each do |line|
   case line
   when /\Ainitial state: ([#.]+)\z/
-    Regexp.last_match(1).each_char.with_index do |c, i|
-      @pots << i if c == '#'
-    end
+    str = Regexp.last_match(1)
+    @max_pot = str.length - 1
+    @pots = potstr_to_i(str)
   when ''
     # Ignore
   when /\A([#.]{5}) => ([#.])\z/
     if Regexp.last_match(2) == '#'
-      state = []
-      Regexp.last_match(1).each_char.with_index do |c, i|
-        state << i-2 if c == '#'
-      end
-      raise 'Space would explode' if state.empty?
-      @transforms[state] = true
+      @transforms[potstr_to_i(Regexp.last_match(1))] = true
     end
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-pots = @pots
-sum = pots.sum
-diff = nil
-repeats = 0
-i = 0
-# Loop until sum difference converges
-begin
-  last_diff = diff
-  last_sum = sum
+raise "Space would explode with these transformations" if @transforms[0]
 
-  next_pots = Set[]
-  (pots.min-2).upto(pots.max+2) do |pot|
-    state = []
-    -2.upto(2) do |offset|
-      state << offset if pots.include?(pot + offset)
-    end
-    if @transforms[state]
-      next_pots << pot
+# Add 4 pots at each end to check all possible patterns at start and end.
+pots = @pots << 4
+min_pot = -4
+max_pot = @max_pot + 4
+mask = 0b11111
+seen = Hash.new
+i = 0
+while i < GENERATIONS.max
+  next_pots = 0
+  (max_pot - min_pot + 1).times do |i|
+    shift = i - 2
+    if @transforms[(pots & (mask << shift)) >> shift]
+      next_pots |= 1 << i
+      if pots >> i == 0
+        max_pot += 1
+      end
     end
   end
   pots = next_pots
-  sum = pots.sum
-
+  # Shift cluster until the four lowest bits (and no more) are unused.
+  while pots & 0b1111 != 0
+    pots <<= 1
+    min_pot -= 1
+  end
+  while pots & 0b11111 == 0
+    pots >>= 1
+    min_pot += 1
+  end
   i+= 1
+
   if GENERATIONS.include?(i)
-    # Part 1
-    puts "Sum after #{i} generations: #{sum}"
+    puts "Sum after #{i} generations: #{pots_sum(pots, min_pot)}"
   end
-  diff = sum - last_sum
-
-  if diff == last_diff
-    repeats += 1
-  else
-    repeats = 0
+  if seen.has_key?(pots)
+    # Glider state (or equilibrium) reached. Further states can be extrapolated.
+    break
   end
-end while repeats < 20
+  seen[pots] = [min_pot, i]
+end
 
-GENERATIONS.each do |g|
-  next if g <= i
-  puts "Sum after #{g} generations: #{sum + (g - i) * diff}"
+if i < GENERATIONS.max
+  seen_min, seen_i = seen[pots]
+  min_diff = seen_min - min_pot
+  i_diff = seen_i - i
+  GENERATIONS.each do |g|
+    next if g <= i
+    g_min = min_pot + (min_diff * (g - i) / i_diff.to_f).to_i
+    puts "Sum after #{g} generations: #{pots_sum(pots, g_min)}"
+  end
 end
