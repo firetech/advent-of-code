2c87410f7cc3f5119b64a8425c6ec25e5d3e5f76
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-11-30 01:49:04 +0100

    2018/25 completed.
    ..aaand, there. I've now completed the entire backlog, with just over 28
    hours to spare before the 2021 calendar unlocks its first puzzle.

---
+++ 2018/25/constellations.rb
@@ -0,0 +1,38 @@
+require 'set'
+
+file = 'input'
+#file = 'example1'
+#file = 'example2'
+#file = 'example3'
+#file = 'example4'
+
+@points = File.read(file).strip.split("\n").map do |line|
+  if line =~ /\A(-?\d+),(-?\d+),(-?\d+),(-?\d+)\z/
+    Regexp.last_match.to_a[1..-1].map(&:to_i)
+  else
+    raise "Malformed line: '#{line}'"
+  end
+end
+
+def distance(a, b)
+  a.zip(b).map { |aa, bb| (aa - bb).abs }.sum
+end
+
+@groups = {}
+@points.each do |point|
+  @groups[point] = Set[point]
+end
+@points.combination(2) do |a, b|
+  if distance(a, b) <= 3
+    a_group = @groups[a]
+    b_group = @groups[b]
+    if a_group != b_group
+      b_group.each do |point|
+        a_group << point
+        @groups[point] = a_group
+      end
+    end
+  end
+end
+num_groups = @groups.values.uniq { |g| g.object_id }.length
+puts "Found #{num_groups} constellations"
