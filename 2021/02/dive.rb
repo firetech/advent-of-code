d36e6b5c019408e2f547f4ed650de43cd65e50c4
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-02 06:19:19 +0100

    2021/02 completed.
    Global rank 495/1343 today. Screwed up my first answer for part 2 by
    multiplying aim with the x (horizontal) value instead of n (the input)
    when increasing y (the vertical value)...

---
+++ 2021/02/dive.rb
@@ -0,0 +1,29 @@
+file = 'input'
+#file = 'example1'
+
+x = 0
+y1 = 0
+y2 = 0
+@commands = File.read(file).strip.split("\n").map do |line|
+  case line
+  when /\A(up|down|forward) (\d+)\z/
+    n = Regexp.last_match(2).to_i
+    case Regexp.last_match(1)
+    when 'up'
+      y1 -= n
+    when 'down'
+      y1 += n
+    when 'forward'
+      x += n
+      y2 += y1 * n # y1 is equivalent to aim
+    end
+  else
+    raise "Malformed line: '#{line}'"
+  end
+end
+
+# Part 1
+puts "X/Y movement: #{x} * #{y1} = #{x * y1}"
+
+# Part 2
+puts "X/Aim movement: #{x} * #{y2} = #{x * y2}"
