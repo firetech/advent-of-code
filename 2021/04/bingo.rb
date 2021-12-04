c60e48249d69e8ec4f55060362400e7d83402c0d
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-04 06:19:54 +0100

    2021/04 completed.
    My top result ever, rank 165 on part 1 and 328 on part 2. :D

---
+++ 2021/04/bingo.rb
@@ -0,0 +1,44 @@
+require 'set'
+
+file = 'input'
+#file = 'example1'
+
+numbers_str, *board_strs = File.read(file).strip.split("\n\n")
+
+@numbers = numbers_str.split(',').map(&:to_i)
+@boards = board_strs.map do |board|
+  board.split("\n").map { |line| line.strip.split(/\s+/).map(&:to_i) }
+end
+
+def check_board(board, drawn)
+  (board + board.transpose).any? { |line| line.all? { |n| drawn.include?(n) } }
+end
+
+def print_score(board, drawn, last_number)
+  puts board.flatten.select { |n| not drawn.include?(n) }.sum * last_number
+end
+
+boards = @boards
+drawn = Set[]
+winning_board = nil
+@numbers.each do |n|
+  drawn << n
+  if drawn.length >= 5
+    if winning_board.nil?
+      winning_board = @boards.find { |board| check_board(board, drawn) }
+      unless winning_board.nil?
+        print "Score of first winning board: "
+        print_score(winning_board, drawn, n)
+      end
+    end
+    last_boards = boards
+    boards = boards.reject { |board| check_board(board, drawn) }
+    if boards.empty?
+      raise "Multiple last winners" if boards.length > 1
+      print "Score of last winning board: "
+      print_score(last_boards.first, drawn, n)
+      break
+    end
+  end
+end
+
