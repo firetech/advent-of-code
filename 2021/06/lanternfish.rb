fddd1f2dbe06de54270962571a85e777d47f2924
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-06 06:33:52 +0100

    2021/06 completed.

---
+++ 2021/06/lanternfish.rb
@@ -0,0 +1,25 @@
+file = 'input'
+#file = 'example1'
+
+DAYS = [
+  80, # Part 1
+  256 # Part 2
+]
+
+fish = Hash.new(0)
+File.read(file).strip.split(',').map(&:to_i).each { |f| fish[f] += 1 }
+@fish = [fish]
+
+DAYS.max.times do
+  new_fish = @fish.last.transform_keys { |f| f - 1 }
+  n_new = new_fish.delete(-1)
+  unless n_new.nil?
+    new_fish[6] = (new_fish[6] or 0) + n_new
+    new_fish[8] = n_new
+  end
+  @fish << new_fish
+end
+
+DAYS.each do |days|
+  puts "Lanternfish after #{days} days: #{@fish[days].values.sum}"
+end
