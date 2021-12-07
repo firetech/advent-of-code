dd07e046b971160da5fc11c8b83c1407614daf04
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-07 06:17:32 +0100

    2021/07 completed.

---
+++ 2021/07/crabsubs.rb
@@ -0,0 +1,17 @@
+file = 'input'
+#file = 'example1'
+
+@crabs = File.read(file).strip.split(',').map(&:to_i)
+
+fuels = @crabs.min.upto(@crabs.max).map do |pos|
+  [
+    @crabs.sum { |c| (c - pos).abs },                # Part 1
+    @crabs.sum { |c| n = (c - pos).abs; n*(n+1)/2 }  # Part 2
+  ]
+end
+
+# Part 1
+puts "Minimum fuel required (1 fuel per step): #{fuels.map(&:first).min}"
+
+# Part 2
+puts "Minimum fuel required (N fuel per step): #{fuels.map(&:last).min}"
