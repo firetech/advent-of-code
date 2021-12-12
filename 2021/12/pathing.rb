229339c5e3b84da02fa8196e084a2e5d6e4ab4d8
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-12 09:24:10 +0100

    Improve 2021/12.
    * Using DFS (stack) instead of BFS (queue) saves some time. Probably
      because .pop is faster than .shift.
    * No need to keep track of the exact paths taken, all that's needed is
      the list of visited small caves.
    * Count the times end is reached instead of storing the paths and
      counting (and filtering) afterwards.
    * Don't store routes back to start, as they're never used.
    * Consistency fix, use [A-Z] instead of [[:upper:]] in traversal.
    * Restructure traversal loop slightly.
    
    After this, the code runs just over three times faster than before.

--- 2021/12/pathing.rb
+++ 2021/12/pathing.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 file = 'input'
 #file = 'example1'
 #file = 'example2'
@@ -8,45 +6,49 @@ file = 'input'
 
 File.read(file).strip.split("\n").each do |line|
   case line
-  when /\A([a-zA-Z]+)-([a-zA-Z]+)\z/
+  when /\A([a-z]+|[A-Z]+)-([a-z]+|[A-Z]+)\z/
     a = Regexp.last_match(1)
     b = Regexp.last_match(2)
     @nodes[a] ||= []
     @nodes[b] ||= []
-    @nodes[a] << b
-    @nodes[b] << a
+    @nodes[a] << b unless b == 'start'
+    @nodes[b] << a unless a == 'start'
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
 print "Traversing..."
-queue = [['start', ['start'], nil]]
-paths = []
+stack = [['start', ['start'], nil]]
 i = 0
-while not queue.empty?
-  print '.' if (i += 1) % 10000 == 0
-
-  pos, path, double = queue.shift
+paths = 0 # Part 1
+double_paths = 0 # Part 2
+while not stack.empty?
+  pos, visited, double = stack.pop
   @nodes[pos].each do |cave|
-    cave_path = path + [cave]
     if cave == 'end'
-      paths << [cave_path, double]
+      paths += 1 if double.nil?
+      double_paths += 1
     else
-      if path.include?(cave) and not cave =~ /\A[[:upper:]]+\z/
-        if double.nil? and cave != 'start'
-          queue << [cave, cave_path, cave]
+      cave_double = double
+      cave_visited = visited
+      unless cave =~ /\A[A-Z]+\z/
+        if visited.include?(cave)
+          next unless double.nil?
+          cave_double = cave
+        else
+          cave_visited += [cave]
         end
-      else
-        queue << [cave, cave_path, double]
       end
+      stack << [cave, cave_visited, cave_double]
     end
   end
 end
 puts " Done."
+puts
 
 # Part 1
-puts "Possible paths: #{paths.map(&:last).count(nil)}"
+puts "Possible paths: #{paths}"
 
 # Part 2
-puts "Possible paths with one small cave visited at most twice: #{paths.length}"
+puts "Possible paths with one small cave visited at most twice: #{double_paths}"
