26a248d91575379f9a9ba36e1a5c797138534db4
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-14 11:39:54 +0100

    Speed up 2021/14.
    Not really needed, but working with strings instead of arrays of
    characters (which are strings of length 1) speeds up the code ~5x.

--- 2021/14/polymerization.rb
+++ 2021/14/polymerization.rb
@@ -12,7 +12,7 @@ STEPS = [
 rules_str.split("\n").each do |line|
   case line
   when /\A([A-Z]+) -> ([A-Z])\z/
-    @rules[Regexp.last_match(1).chars] = Regexp.last_match(2)
+    @rules[Regexp.last_match(1)] = Regexp.last_match(2)
   else
     raise "Malformed line: '#{line}'"
   end
@@ -20,17 +20,17 @@ end
 
 pairs = Hash.new(0)
 @template.chars.each_cons(2) do |pair|
-  pairs[pair] += 1
+  pairs[pair.join] += 1
 end
 STEPS.max.times do |i|
   new = Hash.new(0)
   pairs.each do |pair, count|
-    rule = @rules[pair]
-    if rule.nil?
+    add = @rules[pair]
+    if add.nil?
       new[pair] += count
     else
-      new[[pair.first, rule]] += count
-      new[[rule, pair.last]] += count
+      new[pair[0] + add] += count
+      new[add + pair[1]] += count
     end
   end
   pairs = new
@@ -38,10 +38,11 @@ STEPS.max.times do |i|
   if STEPS.include?(i+1)
     counts = Hash.new(0)
     pairs.each do |pair, count|
-      counts[pair.first] += count
-      counts[pair.last] += count
+      counts[pair[0]] += count
+      counts[pair[1]] += count
     end
-    # All characters except the first and last are double counted
+    # All characters except the first and last are double counted by being
+    # included in two pairs (once as first character, once as second character).
     counts[@template[0]] += 1
     counts[@template[-1]] += 1
     values = counts.values
