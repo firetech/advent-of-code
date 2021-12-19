e81ddeb1187b4492a71e58ac77a47b2bd71513e1
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-19 13:48:58 +0100

    Rework 2021/19.
    First fingerprints the distances between each identified beacon (by
    scanner). This fingerprint consists of the absolute distance between the
    points, separated by axis and sorted.
    When trying to match beacon positions between scanners, the algorithm
    tries to match one beacon from each scanner that have identical
    relations to 11 other beacons each. These beacons (and their relations)
    are then used to calculate rotation matrix and scanner positions.
    
    Multicore is a bit overkill now, but it still about halves the runtime,
    so I left it in in.

--- 2021/19/scanners.rb
+++ 2021/19/scanners.rb
@@ -1,17 +1,129 @@
-require 'matrix'
 require 'set'
-require '../../lib/multicore'
+require_relative '../../lib/multicore'
 
 file = 'input'
 #file = 'example1'
+
+class Beacon
+  attr_reader :id, :pos, :relations
+
+  def initialize(id, pos)
+    @id = id
+    @pos = pos
+    @relations = []
+  end
+
+  def delta_to(other)
+    @pos.zip(other.pos).map { |p, o| p - o }
+  end
+
+  def relate_to!(other)
+    delta = delta_to(other).map(&:abs)
+    fingerprint = delta.sort.hash
+    @relations[other.id] = other.relations[@id] = fingerprint
+  end
+
+  def match(other)
+    matches = []
+    @relations.each_with_index do |rel, i|
+      next if rel.nil?  # The relation with self is not really helpful
+      other_i = other.relations.index(rel)
+      matches << [i, other_i] unless other_i.nil?
+    end
+    return matches
+  end
+
+  def rotate!(matrix)
+    @pos = [
+      @pos[0] * matrix[0][0] + @pos[1] * matrix[1][0] + @pos[2] * matrix[2][0],
+      @pos[0] * matrix[0][1] + @pos[1] * matrix[1][1] + @pos[2] * matrix[2][1],
+      @pos[0] * matrix[0][2] + @pos[1] * matrix[1][2] + @pos[2] * matrix[2][2]
+    ]
+  end
+
+  def translate!(delta)
+    @pos = @pos.zip(delta).map { |a, b| a + b }
+  end
+end
+
+class Scanner
+  attr_reader :id, :beacons
+  attr_accessor :pos
+
+  def initialize(id)
+    @id = id
+    @pos = [0, 0, 0]
+    @beacons = []
+  end
+
+  def <<(beacon_pos)
+    new_beacon = Beacon.new(@beacons.length, beacon_pos)
+    @beacons.each do |beacon|
+      new_beacon.relate_to!(beacon)
+    end
+    @beacons << new_beacon
+  end
+
+  def match(other)
+    other.beacons.each do |other_beacon|
+      @beacons.each do |my_beacon|
+        matches = my_beacon.match(other_beacon)
+        if matches.length >= 11  # == 12 matches including the compared beacons
+          return my_beacon, other_beacon, matches
+        end
+      end
+    end
+    return nil
+  end
+
+  def align_to!(other)
+    my_beacon, other_beacon, matches = match(other)
+    return false if my_beacon.nil?
+
+    matches.each do |i, other_i|
+      my_rel = @beacons[i]
+      other_rel = other.beacons[other_i]
+
+      my_delta = my_beacon.pos.zip(my_rel.pos).map { |a, b| a - b }
+      next if my_delta.map(&:abs).uniq.length < 3
+      other_delta = other_beacon.pos.zip(other_rel.pos).map { |a, b| a - b }
+
+      rot_matrix = Array.new(3) do |row|
+        Array.new(3) do |col|
+          my_val = my_delta[row]
+          other_val = other_delta[col]
+          if my_val == other_val
+            1
+          elsif my_val == -other_val
+            -1
+          else
+            0
+          end
+        end
+      end
+
+      @beacons.each do |beacon|
+        beacon.rotate!(rot_matrix)
+      end
+      @pos = other_beacon.delta_to(my_beacon)
+      @beacons.each do |beacon|
+        beacon.translate!(@pos)
+      end
+      break
+    end
+    return true
+  end
+end
+
 
 @scanners = {}
 curr_scanner = nil
 File.read(file).strip.split("\n").each do |line|
   case line
   when /\A--- scanner (\d+) ---\z/
-    curr_scanner = []
-    @scanners[Regexp.last_match(1).to_i] = curr_scanner
+    id = Regexp.last_match(1).to_i
+    curr_scanner = Scanner.new(id)
+    @scanners[id] = curr_scanner
   when /\A(-?\d+),(-?\d+),(-?\d+)\z/
     raise "Unexpected coordinates" if curr_scanner.nil?
     curr_scanner << Regexp.last_match.to_a[1..-1].map(&:to_i)
@@ -22,79 +134,42 @@ File.read(file).strip.split("\n").each do |line|
   end
 end
 
-FACING = [
-  Vector[ 1,  0,  0],
-  Vector[-1,  0,  0],
-  Vector[ 0,  1,  0],
-  Vector[ 0, -1,  0],
-  Vector[ 0,  0,  1],
-  Vector[ 0,  0, -1]
-]
-
-ROTATIONS = FACING.flat_map do |facing|
-  matrixes = []
-  FACING.each do |up|
-    unless facing.zip(up).all? { |f, u| f.abs == u.abs }
-      right = facing.cross_product(up)
-      matrixes << Matrix[facing, up, right]
-    end
-  end
-  matrixes
-end
-
-@rotations = {}
-@scanners.each do |id, beacons|
-  next if id == 0  # 0 is used as base, no need to rotate it
-  @rotations[id] = ROTATIONS.map do |rot_matrix|
-    beacons.map { |pos| (rot_matrix * Vector.elements(pos)).to_a }
-  end
-end
-
-def rebase_scanner(id, beacons)
-  @rotations[id].each do |rotated_scanner|
-    matching = {}
-    rotated_scanner.each_with_index do |rpos, i|
-      beacons.each do |aligned_pos|
-        delta = aligned_pos.zip(rpos).map { |a, r| a - r }
-        match = (matching[delta] ||= Set[])
-        next if match.include?(i)
-        match << i
-        if match.length >= 12
-          translated_scanner = rotated_scanner.map do |pos|
-            pos.zip(delta).map { |p, d| p + d }
-          end
-          return id, delta, translated_scanner
+aligned_scanners = Set[0]
+tried = Set[]
+@beacons = Set.new(@scanners[0].beacons.map(&:pos))
+begin
+  input, output, stop = Multicore.run do |worker_in, worker_out, _, _|
+    loop do
+      scanner, candidates = worker_in[]
+      break if scanner.nil?
+      aligned = false
+      candidates.each do |aligned_scanner|
+        if scanner.align_to!(aligned_scanner)
+          aligned = true
+          break
         end
       end
+      worker_out[[aligned, scanner]]
     end
   end
-  return id, nil, nil
-end
-
-@beacons = Set.new(@scanners[0])
-@aligned_scanners = { 0 => [0, 0, 0] }
-begin
-  input, output, stop = Multicore.run do |worker_in, worker_out, t, _|
-    loop do
-      id, beacons = worker_in[]
-      break if id.nil?
-      worker_out[rebase_scanner(id, beacons)]
-    end
-  end
-  while @aligned_scanners.length < @scanners.length
+  while aligned_scanners.length < @scanners.length
     inputs_sent = 0
-    @scanners.each_key do |id|
-      next if @aligned_scanners.has_key?(id)
-      input << [id, @beacons]
+    candidate_ids = (aligned_scanners - tried)
+    tried += candidate_ids
+    candidates = candidate_ids.map { |id| @scanners[id] }
+    @scanners.each do |id, scanner|
+      next if aligned_scanners.include?(id)
+      input << [scanner, candidates]
       inputs_sent += 1
     end
     inputs_sent.times do
-      id, scanner_pos, aligned_beacons = output.pop
-      raise "Worker returned nil" if id.nil?
-      unless scanner_pos.nil?
-        puts "Found alignment for scanner #{id}"
-        @beacons += aligned_beacons
-        @aligned_scanners[id] = scanner_pos
+      aligned, scanner = output.pop
+      raise "Worker returned nil" if aligned.nil?
+      if aligned
+        puts "Found alignment for scanner #{scanner.id}"
+        aligned_scanners << scanner.id
+        @scanners[scanner.id] = scanner
+        @beacons += scanner.beacons.map(&:pos)
       end
     end
   end
@@ -102,12 +177,13 @@ begin
 ensure
   stop[] unless stop.nil?
 end
+puts
 
 # Part 1
 puts "Beacons found: #{@beacons.length}"
 
 # Part 2
-distances = @aligned_scanners.values.combination(2).map do |a, b|
+distances = @scanners.values.map(&:pos).combination(2).map do |a, b|
   a.zip(b).map { |aa, bb| (aa - bb).abs }.sum
 end
 puts "Largest manhattan distance between scanners: #{distances.max}"
