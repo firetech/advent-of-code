14b58997bf7e0a66b3b1a1c54946c9c3f240d1d5
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-22 08:54:12 +0100

    2021/22 completed.
    Clean up and add back part 1.

--- 2021/22/cubes.rb
+++ 2021/22/cubes.rb
@@ -22,8 +22,27 @@ class Cuboid
         [@z_min, other.z_min].max < [@z_max, other.z_max].min)
   end
 
+  def intersection(other)
+    return nil unless intersects?(other)
+    return Cuboid.new(
+      [@x_min, other.x_min].max,
+      [@x_max, other.x_max].min,
+      [@y_min, other.y_min].max,
+      [@y_max, other.y_max].min,
+      [@z_min, other.z_min].max,
+      [@z_max, other.z_max].min
+    )
+  end
+
   def slice_with(other)
     return [] unless intersects?(other)
+    # Generate the list of sub-cuboids created by slicing each two cuboids along
+    # each of the other's boundaries.
+    # I.e. (in two dimensions, # = Overlap)
+    #   222       112
+    # 11##2  => 33445
+    # 11##2     33445
+    # 1111      6677
     slices = []
     xs = [@x_min, other.x_min, @x_max, other.x_max].uniq.sort
     ys = [@y_min, other.y_min, @y_max, other.y_max].uniq.sort
@@ -51,14 +70,17 @@ class Cuboid
   end
 
   def eql?(other)
-    return false unless other.is_a?(Cuboid)
-    return (@x_min == other.x_min and @x_max == other.x_max and
+    return (other.is_a?(Cuboid) and
+        @x_min == other.x_min and @x_max == other.x_max and
         @y_min == other.y_min and @y_max == other.y_max and
         @z_min == other.z_min and @z_max == other.z_max)
   end
 end
 
-@on_cuboids = Set[]
+PART1_BOUNDARY = Cuboid.new(-50, 51, -50, 51, -50, 51)
+
+@part1_cuboids = Set[]
+@part2_cuboids = Set[]
 File.read(file).strip.split("\n").each do |line|
   case line
   when /\A(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)\z/
@@ -67,22 +89,36 @@ File.read(file).strip.split("\n").each do |line|
     y_min, y_max = Regexp.last_match(4).to_i, Regexp.last_match(5).to_i + 1
     z_min, z_max = Regexp.last_match(6).to_i, Regexp.last_match(7).to_i + 1
     new_cuboid = Cuboid.new(x_min, x_max, y_min, y_max, z_min, z_max)
-    @on_cuboids.to_a.each do |cuboid|
-      # Slice up intersection into smaller cuboids, add all slices that only
-      # intersect with the old cuboid
-      slices = cuboid.slice_with(new_cuboid)
-      next if slices.empty?
-      @on_cuboids.delete(cuboid)
-      slices.each do |slice|
-        if cuboid.intersects?(slice) and not new_cuboid.intersects?(slice)
-          @on_cuboids << slice
+    [
+      [@part1_cuboids, PART1_BOUNDARY],
+      [@part2_cuboids, nil]
+    ].each do |cuboids, boundary|
+      target_cuboid = new_cuboid
+      unless boundary.nil?
+        target_cuboid = boundary.intersection(target_cuboid)
+        next if target_cuboid.nil?
+      end
+      cuboids.to_a.each do |cuboid|
+        # Slice up intersection into smaller cuboids, add all slices that only
+        # intersect with the old cuboid
+        slices = cuboid.slice_with(target_cuboid)
+        next if slices.empty?
+        cuboids.delete(cuboid)
+        slices.each do |slice|
+          if cuboid.intersects?(slice) and not target_cuboid.intersects?(slice)
+            cuboids << slice
+          end
         end
       end
+      cuboids << target_cuboid if on
     end
-    @on_cuboids << new_cuboid if on
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-pp @on_cuboids.sum(&:size)
+# Part 1
+puts "Cubes on in initialization region: #{@part1_cuboids.sum(&:size)}"
+
+# Part 2
+puts "Cubes on in entire space: #{@part2_cuboids.sum(&:size)}"
