f29fbf2560e9d256d09325ee07f9410d7d63050c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2021-12-23 15:19:50 +0100

    Further improvement of 2021/23.
    * Improve occupant checking by looping over the list of amphipods
      once instead of once per coordinate.
    * Improve heuristic by instead calculating the minimum cost to move all
      amphipods to their column, ignoring any collisions. Also use this
      method to check if done (it returns 0).
    * Remove (now) unused debugging methods.
    * Cleanup some naming and add some more comments.
    
    Now finishes within 15 seconds on a 5950X, not sure if it can be sped up
    even more or not.

--- 2021/23/amphipods.rb
+++ 2021/23/amphipods.rb
@@ -1,4 +1,3 @@
-require 'set'
 require_relative '../../lib/priority_queue'
 
 file = 'input'
@@ -6,158 +5,129 @@ file = 'input'
 #file = 'custom_example'
 
 COST = { A: 1, B: 10, C: 100, D: 1000 }
-WAITING_X = Set[1, 2, 4, 6, 8, 10, 11]
+WAITING_X = [1, 2, 4, 6, 8, 10, 11]
 END_X = { A: 3, B: 5, C: 7, D: 9 }
 
-@pods = []
-@map = File.read(file).strip.split("\n").map.with_index do |line, y|
-  line.chars.map.with_index do |c, x|
+@amphipods = []
+File.read(file).strip.split("\n").each_with_index do |line, y|
+  line.each_char.with_index do |c, x|
     case c
     when '#', ' ', '.'
-      c
+      # Ignore
     when /\A[A-D]\z/
-      @pods << [x, y, c.to_sym]
-      '.'
+      @amphipods << [x, y, c.to_sym]
     else
       raise "Malformed line: '#{line}'"
     end
   end
 end
 
-def done?(pods, end_y)
-  pods.all? do |x, y, type|
-    x == END_X[type] and end_y.include?(y)
-  end
-end
-
-def occupant(x, y, pods)
-  pods.each do |px, py, type|
-    if px == x and py == y
-      return type
-    end
-  end
-  return nil
-end
-
-def print_state(pods, end_y)
-  offset = 0
-  @map.each_with_index do |line, y|
-    times = (y == 3) ? end_y.max - 2 : 1
-    times.times do |t|
-      line.each_with_index do |m, x|
-        pod = occupant(x, y + offset + t, pods)
-        if pod.nil?
-          print m
-        else
-          print pod
-        end
-      end
-      puts
-    end
-    offset += times - 1
-  end
-end
-
 TYPE_HASH = { A: 0, B: 1, C: 2, D: 3 }
-def state_hash(pods)
+def state_hash(amphipods)
   value = 0
-  pods.each do |x, y, type|
-    #                     4 bits   3 bits   2 bits
+  amphipods.each do |x, y, type|
+    #                    4 bits   3 bits   2 bits
     value = value << 9 | x << 5 | y << 2 | TYPE_HASH[type]
   end
   return value
 end
 
-def heuristic(x, y, type)
-  if y == 1 # Moving to hall, calculate cost of move to top of home column
-    return ((x - END_X[type]).abs + 1) * COST[type]
-  else # Moving from hall, no extra cost
-    return 0
+def min_done_cost(amphipods)
+  # Minimum cost to move all amphipods to their column (ignoring collisions)
+  cost = 0
+  amphipods.each do |x, y, type|
+    home = END_X[type]
+    next if x == home
+    # Move to corridor, to above home column and down into it
+    cost += ((y - 1).abs + (x - home).abs + 1) * COST[type]
   end
+  return cost
 end
 
-def move_to_order(pods, end_y)
+def move_to_order(amphipods, end_y)
   # Dijkstra adapted from 2021/15, turned into A*.
-  hash = state_hash(pods)
-  map = { hash => pods }
+  hash = state_hash(amphipods)
+  map = { hash => amphipods }
   cost = Hash.new(Float::INFINITY)
   cost[hash] = 0
   queue = PriorityQueue.new
   queue.push(hash, 0)
-  cheapest = nil
   until queue.empty?
     hash = queue.pop_min
     this_cost = cost[hash]
     state = map[hash]
 
-    if done?(state, end_y)
+    if min_done_cost(state) == 0
       return this_cost
     end
 
     state.each_with_index do |(x, y, type), i|
       moves = []
+      home = END_X[type]
       if y == 1 # In corridor
-        # Skip pods that can't move home
-        home_bottom = nil
-        end_y.max.downto(end_y.min) do |ey| # Loop from bottom up
-          pod = occupant(END_X[type], ey, state)
-          if pod.nil?
-            home_bottom = ey
-            break
-          elsif pod != type
+        # Find lowest free position in home column
+        home_has_other = false
+        home_y = end_y.max
+        state.each do |other_x, other_y, other_type|
+          next if other_x != home
+          if other_type != type
+            # Can't move home yet, other type is in home column
+            home_has_other = true
             break
           end
+          home_y = other_y - 1 if other_y <= home_y
         end
-        next if home_bottom.nil?
-        moves << [END_X[type], home_bottom]
+        next if home_has_other
+        # Target lowest free position in home column
+        moves << [home, home_y]
       else # In a column
-        # Skip pods that are done
-        if x == END_X[type] # In right column
-          done = true
-          # Not blocking anything else that shouldn't be there
-          end_y.max.downto(y+1) do |ey|
-            if occupant(x, ey, state) != type
-              done = false
-              break
-            end
-          end
-          next if done
-        end
-        # Skip pods that can't move
+        # Skip amphipods that are done or can't move
+        done = (x == home) # In right column
         blocked = false
-        (y-1).downto(end_y.min) do |cy|
-          unless occupant(x, cy, state).nil?
+        home_free = true
+        home_pos = end_y.max
+        state.each do |other_x, other_y, other_type|
+          next if other_x != x
+          if other_y < y
+            # Another amphipod is above
             blocked = true
             break
+          elsif done and other_type != type and other_y > y
+            # This amphipod is home, but an amphipod of wrong type is below
+            done = false
           end
         end
-        next if blocked
-        # Check all possible waiting positions
-        WAITING_X.each do |wx|
-          moves << [wx, 1] if occupant(wx, 1, state).nil?
-        end
+        next if done or blocked
+        # Target all possible corridor positions
+        WAITING_X.each { |wx| moves << [wx, 1] }
       end
       moves.each do |mx, my|
+        # Check if any other amphipod is in the way in the corridor
         free_path = true
-        [x, mx].min.upto([x, mx].max) do |cx|
-          next if x == cx
-          next unless WAITING_X.include?(cx)
-          unless occupant(cx, 1, state).nil?
+        x_limits = [x, mx]
+        x_min = x_limits.min
+        x_max = x_limits.max
+        state.each do |other_x, other_y, _|
+          next if other_y != 1 # Other is not in corridor
+          next if other_x == x # Other is the moving amphipod
+          if other_x >= x_min and other_x <= x_max
+            # Other amphipod is in the way
             free_path = false
             break
           end
         end
         next unless free_path
 
+        # Move is possible, calculate cost and add to queue (A*).
         new_state = state.map.with_index do |pod, new_i|
           (new_i == i) ? [mx, my, type] : pod
         end
         new_hash = state_hash(new_state)
-        map[new_hash] = new_state
         new_cost = this_cost + ((x - mx).abs + (y - my).abs) * COST[type]
         if new_cost < cost[new_hash]
+          map[new_hash] = new_state
           cost[new_hash] = new_cost
-          queue.push(new_hash, new_cost + heuristic(mx, my, type))
+          queue.push(new_hash, new_cost + min_done_cost(new_state))
         end
       end
     end
@@ -166,12 +136,11 @@ def move_to_order(pods, end_y)
 end
 
 # Part 1
-puts "Least energy to order: #{move_to_order(@pods, 2..3)}"
+puts "Least energy to order: #{move_to_order(@amphipods, 2..3)}"
 
 # Part 2
-new_pods = [
+part2_amphipods = [
   [3, 3, :D], [5, 3, :C], [7, 3, :B], [9, 3, :A],
   [3, 4, :D], [5, 4, :B], [7, 4, :A], [9, 4, :C]
-]
-part2_pods = @pods.map { |x, y, type| [x, (y == 3 ? 5 : y), type] } + new_pods
-puts "Least energy to order (unfolded): #{move_to_order(part2_pods, 2..5)}"
+] + @amphipods.map { |x, y, type| [x, (y == 3 ? 5 : y), type] }
+puts "Least energy to order (unfolded): #{move_to_order(part2_amphipods, 2..5)}"
