f5292545ccb4ff4201b23de9308c24ead9742799
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-01 06:08:58 +0100

    2022/01 completed.
    Here we go again.

---
+++ 2022/01/calories.rb
@@ -0,0 +1,12 @@
+file = 'input'
+#file = 'example1'
+
+@elves = File.read(file).strip.split("\n\n").map do |group|
+  group.split("\n").map(&:to_i).sum
+end
+
+# Part 1
+puts "Top elf is carrying #{@elves.max} Calories"
+
+# Part 2
+puts "Top three elves are carrying #{@elves.sort.last(3).flatten.sum} Calories"
