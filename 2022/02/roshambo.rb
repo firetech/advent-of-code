10b139c97af840f763a550dad82ddd56f9bb2cdf
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-02 10:52:12 +0100

    Even more simplified 2022/02.
    Solved with just one loop and some math.

--- 2022/02/roshambo.rb
+++ 2022/02/roshambo.rb
@@ -1,46 +1,36 @@
 file = ARGV[0] || 'input'
 #file = 'example1'
 
-@strategy = []
+@strategy = Hash.new(0)
 File.read(file).strip.split("\n").each do |line|
   case line
   when /\A(A|B|C) (X|Y|Z)\z/
-    @strategy << [
-      Regexp.last_match(1).ord - 'A'.ord,
-      Regexp.last_match(2).ord - 'X'.ord
-    ]
+    @strategy[line] += 1
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
 # Part 1
-# me (and opp): 0 = rock, 1 = paper, 2 = scissors
-@score1 = 0
-@strategy.each do |opp, me|
-  @score1 += me + 1
-  if me == opp
-    @score1 += 3
-  elsif me == (opp+1)%3
-    @score1 += 6
-  end
-end
-puts "XYZ means RPS score: #{@score1}"
+@score1 = @strategy['A X'] * (1 + 3) + # Rock     <- Rock     => Draw
+          @strategy['A Y'] * (2 + 6) + # Rock     <- Paper    => Win
+          @strategy['A Z'] * (3 + 0) + # Rock     <- Scissors => Loss
+          @strategy['B X'] * (1 + 0) + # Paper    <- Rock     => Loss
+          @strategy['B Y'] * (2 + 3) + # Paper    <- Paper    => Draw
+          @strategy['B Z'] * (3 + 6) + # Paper    <- Scissors => Win
+          @strategy['C X'] * (1 + 6) + # Scissors <- Rock     => Win
+          @strategy['C Y'] * (2 + 0) + # Scissors <- Paper    => Loss
+          @strategy['C Z'] * (3 + 3)   # Scissors <- Scissors => Draw
+puts "XYZ means Rock/Paper/Scissors => score: #{@score1}"
 
 # Part 2
-# me: 0 = lose, 1 = draw, 2 = win  (opp: see part 1)
-@score2 = 0
-@strategy.each do |opp, me|
-  move = nil
-  case me
-  when 0 # lose
-    move = opp + 2
-  when 1 # draw
-    move = opp
-  when 2 # win
-    move = opp + 1
-  end
-  #          move sc. | result sc.
-  @score2 += move%3+1 + 3*me
-end
-puts "XYZ means lose/draw/win score: #{@score2}"
+@score2 = @strategy['A X'] * (3 + 0) + # Rock     <- Loss => Scissors
+          @strategy['A Y'] * (1 + 3) + # Rock     <- Draw => Rock
+          @strategy['A Z'] * (2 + 6) + # Rock     <- Win  => Paper
+          @strategy['B X'] * (1 + 0) + # Paper    <- Loss => Rock
+          @strategy['B Y'] * (2 + 3) + # Paper    <- Draw => Paper
+          @strategy['B Z'] * (3 + 6) + # Paper    <- Win  => Scissors
+          @strategy['C X'] * (2 + 0) + # Scissors <- Loss => Paper
+          @strategy['C Y'] * (3 + 3) + # Scissors <- Draw => Scissors
+          @strategy['C Z'] * (1 + 6)   # Scissors <- Win  => Rock
+puts "XYZ means Lose/Draw/Win => score: #{@score2}"
