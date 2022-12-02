74b907911d03bd10d0da3cb09f2da7d0eb816d41
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-02 06:40:56 +0100

    Less verbose solution to 2022/02.

--- 2022/02/roshambo.rb
+++ 2022/02/roshambo.rb
@@ -1,31 +1,13 @@
 file = ARGV[0] || 'input'
 #file = 'example1'
 
-MAP = {
-  A: :rock,
-  B: :paper,
-  C: :scissors,
-}
-
-SCORE = {
-  rock: 1,
-  paper: 2,
-  scissors: 3
-}
-
-WIN_OVER = {
-  rock: :scissors,
-  scissors: :paper,
-  paper: :rock
-}
-
 @strategy = []
 File.read(file).strip.split("\n").each do |line|
   case line
   when /\A(A|B|C) (X|Y|Z)\z/
     @strategy << [
-      MAP[Regexp.last_match(1).to_sym],
-      Regexp.last_match(2).to_sym
+      Regexp.last_match(1).ord - 'A'.ord,
+      Regexp.last_match(2).ord - 'X'.ord
     ]
   else
     raise "Malformed line: '#{line}'"
@@ -33,40 +15,32 @@ File.read(file).strip.split("\n").each do |line|
 end
 
 # Part 1
-PART1_MAP = {
-  X: :rock,
-  Y: :paper,
-  Z: :scissors
-}
-@part1_score = 0
-
-# Part 2
-@part2_score = 0
-
+# me (and opp): 0 = rock, 1 = paper, 2 = scissors
+@score1 = 0
 @strategy.each do |opp, me|
-  # Part 1
-  me1 = PART1_MAP[me]
-  @part1_score += SCORE[me1]
-  if opp == me1
-    @part1_score += 3
-  elsif WIN_OVER[me1] == opp
-    @part1_score += 6
-  end
-
-  # Part 2
-  case me
-  when :X
-    @part2_score += SCORE[WIN_OVER[opp]]
-  when :Y
-    @part2_score += SCORE[opp] + 3
-  when :Z
-    @part2_score += SCORE[WIN_OVER.invert[opp]] + 6
+  @score1 += me + 1
+  if me == opp
+    @score1 += 3
+  elsif me == (opp+1)%3
+    @score1 += 6
   end
 end
-
-# Part 1
-puts "XYZ means RPS score: #{@part1_score}"
+puts "XYZ means RPS score: #{@score1}"
 
 # Part 2
-puts "XYZ means lose/draw/win score: #{@part2_score}"
-
+# me: 0 = lose, 1 = draw, 2 = win  (opp: see part 1)
+@score2 = 0
+@strategy.each do |opp, me|
+  move = nil
+  case me
+  when 0 # lose
+    move = opp + 2
+  when 1 # draw
+    move = opp
+  when 2 # win
+    move = opp + 1
+  end
+  #          move sc. | result sc.
+  @score2 += move%3+1 + 3*me
+end
+puts "XYZ means lose/draw/win score: #{@score2}"
