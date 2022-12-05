3d7539c0081be81035b63ff9fa0d5bdc18fd37dc
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-05 06:16:54 +0100

    2022/05 completed.
    Rank 1076/763 today. Fairly happy about that result, considering I have
    had a fever for three days now...

---
+++ 2022/05/cratemover.rb
@@ -0,0 +1,40 @@
+file = ARGV[0] || 'input'
+#file = 'example1'
+
+stacks, instructions = File.read(file).rstrip.split("\n\n")
+
+@stacks = []
+stacks.split("\n")[0..-2].each do |line|
+  line.scan(/(?:\[([A-Z])\]|   )(?: |\Z)/).each_with_index do |crate, i|
+    @stacks[i] ||= []
+    next if crate.first.nil?
+    @stacks[i].unshift(crate.first)
+  end
+end
+
+s1 = @stacks.map(&:clone) # Part 1
+s2 = @stacks.map(&:clone) # Part 2
+instructions.split("\n").each do |line|
+  case line
+  when /\Amove (\d+) from (\d+) to (\d+)\z/
+    count = Regexp.last_match(1).to_i
+    from = Regexp.last_match(2).to_i - 1
+    to = Regexp.last_match(3).to_i - 1
+
+    # Part 1
+    count.times do
+      s1[to] << s1[from].pop
+    end
+
+    # Part 2
+    s2[to] += s2[from].pop(count)
+  else
+    raise "Malformed line: '#{line}'"
+  end
+end
+
+# Part 1
+puts "Stack tops when moving individual crates: #{s1.map(&:last).join}"
+
+# Part 2
+puts "Stack tops when moving groups of crates: #{s2.map(&:last).join}"
