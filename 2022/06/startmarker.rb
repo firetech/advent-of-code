e1fd92bf2aa5f78828fc231d4fccc348c6414c9d
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-06 06:13:29 +0100

    2022/06 completed.

---
+++ 2022/06/startmarker.rb
@@ -0,0 +1,18 @@
+file = ARGV[0] || 'input'
+#file = 'example1'
+
+@input = File.read(file).rstrip
+
+def first_uniq_char(length)
+  @input.chars.each_cons(length).with_index do |chars, i|
+    if chars.uniq.length == length
+      return i + length
+    end
+  end
+end
+
+# Part 1
+puts "Characters before first start-of-packet marker: #{first_uniq_char(4)}"
+
+# Part 2
+puts "Characters before first start-of-message marker: #{first_uniq_char(14)}"
