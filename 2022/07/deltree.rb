62a46ecd0bf093d8b70d4a5f65fe8c7ba800f5ce
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-07 09:50:53 +0100

    Slightly cleaner, class based solution to 2022/07.
    Mostly for fun, to see how clean it would become.

--- 2022/07/deltree.rb
+++ 2022/07/deltree.rb
@@ -1,76 +1,107 @@
 file = ARGV[0] || 'input'
 #file = 'example1'
 
-# Build up tree
-@pwd = []
-@tree = {}
-File.read(file).rstrip.split(/^\$ /)[1..-1].each do |step|
-  cmd, output = step.split("\n", 2)
-  case cmd
-  when /\Acd (.*)\z/
-    case Regexp.last_match(1)
-    when '/'
-      @pwd = []
-    when '..'
-      @pwd.pop
-    else
-      @pwd << Regexp.last_match(1)
+# Classes for tree structure
+class TreeDir
+  @@sizes = {}
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @children = {}
+  end
+
+  def <<(child)
+    @@sizes.delete(self)
+    @children[child.name] = child
+  end
+
+  def [](child)
+    @children[child]
+  end
+
+  def size
+    unless @@sizes.has_key?(self)
+      @@sizes[self] = @children.values.map(&:size).sum
     end
-  when /\Als\z/
-    tree = @tree
-    @pwd.each do |path|
-      tree = tree[path]
-    end
-    output.split("\n").each do |line|
-      case line
-      when /\A(\d+) (.*)\z/
-        # File
-        tree[Regexp.last_match(2)] = Regexp.last_match(1).to_i
-      when /\Adir (.*)\z/
-        # Dir
-        tree[Regexp.last_match(1)] = {}
+    return @@sizes[self]
+  end
+
+  def self.all_sizes
+    return @@sizes.values.sort
+  end
+end
+
+class TreeFile
+  attr_reader :name, :size
+
+  def initialize(name, size)
+    @name = name
+    @size = size.to_i
+  end
+end
+
+# Build up tree from input
+@tree = TreeDir.new('/')
+pwd = [@tree]
+last_cmd = nil
+File.read(file).rstrip.split("\n").each do |line|
+  if line =~ /\A\$ (.*)\z/
+    cmd = Regexp.last_match(1)
+    case cmd
+    when /\Acd (.*)\z/
+      dir = Regexp.last_match(1)
+      case dir
+      when '/'
+        pwd = [@tree]
+      when '..'
+        pwd.pop
       else
-        raise "Malformed line: '#{line}'"
+        pwd << pwd.last[Regexp.last_match(1)]
+        raise "Unknown directory 'dir'" if pwd.last.nil?
       end
-    end
-  end
-end
-
-# Get size for each directory (recursively)
-@dir_size = {}
-def path_size(path = [])
-  unless @dir_size.has_key?(path)
-    tree = @tree
-    path.each do |step|
-      tree = tree[step]
-    end
-    if tree.is_a? Integer
-      return tree
+    when 'ls'
+      # Do nothing
     else
-      @dir_size[path] = tree.sum { |k, v| path_size(path + [k]) }
+      raise "Unknown command: '#{cmd}'"
     end
+    last_cmd = cmd
+  elsif last_cmd == 'ls'
+    case line
+    when /\A(\d+) (.*)\z/
+      # File
+      pwd.last << TreeFile.new(Regexp.last_match(2), Regexp.last_match(1))
+    when /\Adir (.*)\z/
+      # Dir
+      pwd.last << TreeDir.new(Regexp.last_match(1))
+    else
+      raise "Malformed ls output: '#{line}'"
+    end
+  else
+    raise "Unexpected line: '#{line}'"
   end
-  return @dir_size[path]
 end
-path_size()
 
+# Precalculate all directory sizes.
+total_size = @tree.size
 
 # Part 1
 sum = 0
-@dir_size.values.each do |val|
+TreeDir.all_sizes.each do |val|
   sum += val if val < 100000
 end
 puts "Sum of directory sizes < 100000: #{sum}"
 
 
 # Part 2
-TOTAL = 70000000
-NEEDED = 30000000
+TOTAL_SPACE  = 70000000
+NEEDED_SPACE = 30000000
 
-free = TOTAL - path_size()
-min_remove = NEEDED - free
+free = TOTAL_SPACE - total_size
+min_remove = NEEDED_SPACE - free
 
-@dir_size.sort_by(&:last).each do |path, size|
+TreeDir.all_sizes.each do |size|
   if size >= min_remove
     puts "Size of smallest directory that frees up enough space: #{size}"
     break
