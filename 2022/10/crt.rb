bf7ce9c80c6c0478ce89686c25016ef24d915c25
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-10 13:31:15 +0100

    Way cleaner solution to 2022/10.
    Now that I've slept a bit more, and gotten some inspiration from others,
    this makes way more sense.

--- 2022/10/crt.rb
+++ 2022/10/crt.rb
@@ -2,68 +2,23 @@ file = ARGV[0] || 'input'
 #file = 'example1'
 #file = 'example2'
 
-@code = File.read(file).rstrip.split("\n").map do |line|
+x = 1
+@x = [x] + File.read(file).rstrip.split("\n").flat_map do |line|
   case line
   when /\Anoop\z/
-    [:noop]
+    [x]
   when /\Aaddx (-?\d+)\z/
-    [:addx, Regexp.last_match(1).to_i]
+    [x, x += Regexp.last_match(1).to_i]
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-# Part 1 variables
-CHECK = [20, 60, 100, 140, 180, 220]
-cycle = 1
-strength_sum = 0
-
-# Part 2 variables
-screen = Array.new(6) { [' '] * 40 }
-screen_x = 0
-screen_y = 0
-
-i = 0
-x = 1
-next_cycle_add = nil
-while screen_y < screen.length
-  # Part 1
-  if CHECK.include?(cycle)
-    strength_sum += cycle * x
-  end
-
-  # Part 2
-  if (x - screen_x).abs <= 1
-    screen[screen_y][screen_x] = "\u2588"
-  end
-
-  if next_cycle_add.nil?
-    instr, value = @code[i]
-    i += 1
-    case instr
-    when :addx
-      raise unless next_cycle_add.nil?
-      next_cycle_add = value
-    when :noop
-      # Do nothing
-    else
-      raise "Unknown instruction #{instr.inspect} at cycle #{cycle}"
-    end
-  else
-    x += next_cycle_add
-    next_cycle_add = nil
-  end
-
-  cycle += 1
-  screen_x += 1
-  if screen_x >= screen.first.length
-    screen_x = 0
-    screen_y += 1
-  end
-end
-
 # Part 1
+strength_sum = [20, 60, 100, 140, 180, 220].map { |c| @x[c-1] * c }.sum
 puts "Sum of interesting signal strengths: #{strength_sum}"
 
 # Part 2
-screen.each { |line| puts line.join }
+screen = @x.map.with_index { |x, i| ((x - (i % 40)).abs <= 1) ? "\u2588" : ' ' }
+screen.pop # Contains more value than needed
+screen.each_slice(40) { |line| puts line.join }
