dc033866b7cac0c320f013944590ba9e8b1e603c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-12 06:57:26 +0100

    Optimize 2022/12.
    Using Complex for position is quick to write, but slow to run. Now uses
    bitmasked integers for storing the position again, which made the code
    run a bit over 3 times faster.

--- 2022/12/hillclimb.rb
+++ 2022/12/hillclimb.rb
@@ -8,40 +8,57 @@ file = ARGV[0] || 'input'
 @end = nil
 @map = File.read(file).strip.split("\n").map.with_index do |line, y|
   chars = line.chars
+
   if not (x = line.index('S')).nil?
     chars[x] = 'a'
-    @start = Complex(x, y)
+    @start = [x, y]
   end
+
   as = (0...chars.length).find_all { |x| chars[x] == 'a' }
-  @start2.push(*as.map { |x| Complex(x, y) })
+  @start2.push(*as.map { |x| [x, y] })
+
   if not (x = line.index('E')).nil?
     chars[x] = 'z'
-    @end = Complex(x, y)
+    @end = [x, y]
   end
+
   chars
 end
 
+@x_bits = Math.log2(@map.first.length - 1).ceil
+@x_mask = (1 << @x_bits) - 1
+def to_pos(x, y)
+  (y << @x_bits) | x
+end
+def from_pos(pos)
+  return (pos & @x_mask), (pos >> @x_bits)
+end
+
 def dijkstra(start = @start)
-  # Copied from 2021/15. ^_^
+  # Copied from 2018/22 and 2021/15. ^_^
   x_range = (0...@map.first.length)
   y_range = (0...@map.length)
   dist = Hash.new(Float::INFINITY)
-  dist[start] = 0
+  start_pos = to_pos(*start)
+  dist[start_pos] = 0
   queue = PriorityQueue.new
-  queue.push(start, 0)
+  queue.push(start_pos, 0)
+  end_pos = to_pos(*@end)
   until queue.empty?
     pos = queue.pop_min
 
-    if pos == @end
-      return dist[@end]
-    end
+    break if pos == end_pos
 
     this_dist = dist[pos]
-    this_height = @map[pos.imag][pos.real].ord
-    [ -1i, 1i, -1, 1 ].each do |delta|
-      npos = pos + delta
-      next unless x_range.include?(npos.real) and y_range.include?(npos.imag)
-      next unless @map[npos.imag][npos.real].ord - this_height <= 1
+    x, y = from_pos(pos)
+    this_height = @map[y][x].ord
+    [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |delta_x, delta_y|
+      nx = x + delta_x
+      next unless x_range.include?(nx)
+      ny = y + delta_y
+      next unless y_range.include?(ny)
+      next unless @map[ny][nx].ord - this_height <= 1
+      npos = to_pos(nx, ny)
       ndist = this_dist + 1
       if ndist < dist[npos]
         dist[npos] = ndist
@@ -50,7 +67,7 @@ def dijkstra(start = @start)
     end
   end
 
-  return dist[@end]  # Should be Infinity if we get here
+  return dist[end_pos]
 end
 
 # Part 1
