359f6ee1d2ce97f7e576ee8fec7bf0e4dc7efc38
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-13 06:53:01 +0100

    2022/13 completed.
    Getting all corner cases took a while today. Therefore, so did cleaning
    the code up afterwards...

---
+++ 2022/13/distress.rb
@@ -0,0 +1,41 @@
+file = ARGV[0] || 'input'
+#file = 'example1'
+
+@pairs = File.read(file).rstrip.split("\n\n").map do |pair|
+  pair.split("\n").map { |line| eval(line) }
+end
+
+def compare(left, right, depth = 0)
+  l_arr = left.is_a?(Array)
+  r_arr = right.is_a?(Array)
+  if not l_arr and not r_arr
+    return left <=> right
+  elsif l_arr and not r_arr
+    return compare(left, [right], depth + 1)
+  elsif not l_arr and r_arr
+    return compare([left], right, depth + 1)
+  else
+    left.zip(right) do|l, r|
+      return 1 if r.nil?
+      cmp = compare(l, r, depth + 1)
+      return cmp if cmp != 0
+    end
+    return left.length <=> right.length
+  end
+  return 0
+end
+
+# Part 1
+indices = []
+@pairs.each_with_index do |(left, right), i|
+  cmp = compare(left, right)
+  raise 'Equal?!' if cmp == 0
+  indices << i + 1 if cmp < 0
+end
+puts "Sum of pair indices in right order: #{indices.sum}"
+
+# Part 2
+dividers = [ [[2]], [[6]] ]
+@list = @pairs.flatten(1) + dividers
+@list.sort! { |a, b| compare(a, b) }
+puts "Decoder key: #{dividers.map{ |x| @list.index(x) + 1 }.inject(:*)}"
