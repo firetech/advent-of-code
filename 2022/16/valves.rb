b15a7f5997edc10f9e53baf41a43459faf52f927
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-16 14:22:38 +0100

    Speed up 2022/16 a bit more.
    Adding some A*-esque heuristic to skip going further if we can't beat
    the current best even by immediately opening all remaining valves cuts
    the runtime in ~half.

--- 2022/16/valves.rb
+++ 2022/16/valves.rb
@@ -14,10 +14,12 @@ File.read(file).rstrip.split("\n").each do |line|
   end
 end
 
+@usable_valves = @map.keys.select { |valve| @map[valve][:flow] > 0 }.sort
+
 @valve_to_valve = {}
 @map.each_key do |valve|
   queue = [valve]
-  dist = { valve => 0 }
+  dist = { valve => 1 }
   until queue.empty?
     pos = queue.shift
     this_dist = dist[pos]
@@ -27,26 +29,39 @@ end
       queue << new_pos
     end
   end
-  dist.delete(valve)
-  dist.reject! { |valve, cost| @map[valve][:flow] == 0 }
-  @valve_to_valve[valve] = dist
+  @valve_to_valve[valve] = dist.select do |valve, _|
+    @usable_valves.include?(valve)
+  end
+end
+
+def traverse(pos, left, time)
+  max_pressure = 0
+  left.each do |new_pos|
+    cost = @valve_to_valve[pos][new_pos]
+    next if cost > time
+    new_time = time - cost
+    this_pressure = @map[new_pos][:flow] * new_time
+    new_left = left - [new_pos]
+    # If we can't beat the current max by turning on all remaining valves NOW,
+    # there's no need to go further.
+    left_pressure = new_left.sum { |valve| @map[valve][:flow] } * new_time
+    next if this_pressure + left_pressure < max_pressure
+    new_pressure = this_pressure + yield(new_pos, new_left, new_time)
+    if new_pressure > max_pressure
+      max_pressure = new_pressure
+    end
+  end
+  return max_pressure
 end
 
 # Part 1
 @cache = {}
-def dfs(pos = 'AA', open = [], time = 30)
-  state = [pos, open, time].hash
+def dfs(pos = 'AA', left = @usable_valves, time = 30)
+  state = [pos, left, time].hash
   val = @cache[state]
   if val.nil?
-    val = 0
-    @valve_to_valve[pos].each do |new_pos, cost|
-      next if open.include?(new_pos) or cost > time
-      new_time = time - cost - 1
-      new_flow = @map[new_pos][:flow] * new_time
-      new_pressure = new_flow + dfs(new_pos, (open + [new_pos]).sort, new_time)
-      if new_pressure > val
-        val = new_pressure
-      end
+    val = traverse(pos, left, time) do |new_pos, new_left, new_time|
+      dfs(new_pos, new_left, new_time)
     end
     @cache[state] = val
   end
@@ -57,18 +72,21 @@ puts "Most pressure released: #{dfs}"
 
 
 # Part 2
-def dfs2(pos = 'AA', open = [], time = 26)
-  val = dfs('AA', open, 26)
-  @valve_to_valve[pos].each do |new_pos, cost|
-    next if open.include?(new_pos) or cost > time
-    new_time = time - cost - 1
-    new_flow = @map[new_pos][:flow] * new_time
-    new_pressure = new_flow + dfs2(new_pos, (open + [new_pos]).sort, new_time)
-    if new_pressure > val
-      val = new_pressure
-    end
+def dfs2(pos = 'AA', left = @usable_valves, time = 26)
+  val = traverse(pos, left, time) do |new_pos, new_left, new_time|
+    dfs2(new_pos, new_left, new_time)
   end
-  return val
+  return (other = dfs('AA', left, 26)) > val ? other : val
 end
 
-puts "Most pressure released with help: #{dfs2}"
+puts
+print 'Traversing with elephant.'
+t = Thread.new { loop { sleep 1; print '.' } }
+begin
+  val = dfs2
+ensure
+  t.kill
+  t.join
+end
+puts ' Done.'
+puts "Most pressure released with elephant: #{val}"
