67d3a9d53d694a154c2053b55932751ad00a6b12
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-18 08:19:02 +0100

    Optimize 2022/18.
    Not really needed, as the runtime was already < 100ms, but I was
    curious, and felt the previous part 2 implementation was a bit
    convoluted, so now it's just a simple BFS to find uncovered exterior
    sides. Anyway, almost managed to cut the run time in half.

--- 2022/18/lavadroplet.rb
+++ 2022/18/lavadroplet.rb
@@ -3,29 +3,41 @@ require 'set'
 file = ARGV[0] || 'input'
 #file = 'example1'
 
-@voxels = Set[]
-min_x = max_x = 0
-min_y = max_y = 0
-min_z = max_z = 0
+@raw_voxels = []
+MIN_POS = -1 # No negative numbers in input, so this is given
+@max_pos = 1
 File.read(file).rstrip.split("\n").each do |line|
   case line
   when /\A(\d+),(\d+),(\d+)\z/
     x = Regexp.last_match(1).to_i
     y = Regexp.last_match(2).to_i
     z = Regexp.last_match(3).to_i
-    min_x = [min_x, x].min
-    max_x = [max_x, x].max
-    min_y = [min_y, y].min
-    max_y = [max_y, y].max
-    min_z = [min_z, z].min
-    max_z = [max_z, z].max
-    @voxels << [x, y, z]
+    @max_pos = [@max_pos, x+1, y+1, z+1].max
+    @raw_voxels << [x, y, z]
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-def neighbours(x,y,z)
+# Bitmasked integers are faster to hash (for usage in Set).
+@pos_bits = Math.log2(@max_pos + 1).ceil
+@pos_mask = (1 << @pos_bits) - 1
+def to_pos(voxel)
+  voxel.inject(0) { |pos, c| (pos << @pos_bits) | (c + 1) }
+end
+def from_pos(pos)
+  voxel = []
+  3.times do
+    voxel.unshift((pos & @pos_mask) - 1)
+    pos >>= @pos_bits
+  end
+  return voxel
+end
+
+@voxels = Set.new(@raw_voxels.map { |v| to_pos(v) })
+
+def neighbours(pos)
+  x, y, z = from_pos(pos)
   return [
     [x-1, y, z],
     [x+1, y, z],
@@ -37,34 +49,26 @@ def neighbours(x,y,z)
 end
 
 # Part 1
-sum = @voxels.sum(0) do |x, y, z|
-  neighbours(x,y,z).count { |v| not @voxels.include?(v) }
+sum = @voxels.sum do |v|
+  neighbours(v).count { |n| not @voxels.include?(to_pos(n)) }
 end
-
 puts "Surface area of lava droplet: #{sum}"
 
 # Part 2
-all_voxels = Set[]
-(min_x-1..max_x+1).each do |x|
-  (min_y-1..max_y+1).each do |y|
-    (min_z-1..max_z+1).each do |z|
-      all_voxels << [x, y, z]
+queue = [to_pos([MIN_POS, MIN_POS, MIN_POS])]
+visited = Set[]
+area = 0
+until queue.empty?
+  voxel = queue.shift
+  neighbours(voxel).each do |n|
+    next if n.any? { |c| c < MIN_POS or c > @max_pos }
+    n_pos = to_pos(n)
+    if @voxels.include?(n_pos)
+      area += 1
+    elsif not visited.include?(n_pos)
+      visited << n_pos
+      queue << n_pos
     end
   end
 end
-
-# Find enclosed cubes by excluding all non-enclosed empty cubes
-empty = all_voxels - @voxels
-queue = [[min_x-1, min_y-1, min_z-1]]
-until queue.empty?
-  voxel = queue.shift
-  if empty.delete?(voxel)
-    queue.push(*neighbours(*voxel))
-  end
-end
-
-empty_sum = empty.sum(0) do |x, y, z|
-  neighbours(x,y,z).count { |v| not empty.include?(v) }
-end
-
-puts "Exterior surface area of lava droplet: #{sum - empty_sum}"
+puts "Exterior surface area of lava droplet: #{area}"
