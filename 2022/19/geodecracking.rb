9af073d88286fdde687234e61959dc000496125f
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-19 17:11:18 +0100

    Improve 2022/19.
    Cut the runtime in ~half by not allowing building a bot that you could
    have built last minute but didn't. It never makes sense to skip building
    something you can afford and build it the next minute instead.
    This even makes the dubious "if you can build a geode robot, always do
    so" optimization obsolete, which is good, as that is not guaranteed to
    be correct.

--- 2022/19/geodecracking.rb
+++ 2022/19/geodecracking.rb
@@ -16,13 +16,19 @@ File.read(file).rstrip.split("\n").each do |line|
   end
 end
 
+MASK_ORE = 0b0001
+MASK_CLY = 0b0010
+MASK_OBS = 0b0100
+MASK_GEO = 0b1000
+MASK_ALL = MASK_ORE | MASK_CLY | MASK_OBS | MASK_GEO
+
 def run(time, blueprint)
-  stack = [[1,0,0,0, 0,0,0,0, time]]
+  stack = [[1,0,0,0, 0,0,0,0, MASK_ALL, time]]
   max_geo = 0
   c_ore_ore, c_cly_ore, c_obs_ore, c_obs_cly, c_geo_ore, c_geo_obs = blueprint
   max_c_ore = [c_ore_ore, c_cly_ore, c_obs_ore, c_geo_ore].max
   until stack.empty?
-    r_ore, r_cly, r_obs, r_geo, ore, cly, obs, geo, time = stack.pop
+    r_ore, r_cly, r_obs, r_geo, ore, cly, obs, geo, mask, time = stack.pop
 
     max_geo = geo if geo > max_geo
     next if time == 0
@@ -34,44 +40,44 @@ def run(time, blueprint)
     new_cly = cly + r_cly
     new_obs = obs + r_obs
     new_geo = geo + r_geo
-
-    if ore >= c_geo_ore and obs >= c_geo_obs
+    new_mask = mask
+    new_time = time - 1
+    if mask & MASK_GEO != 0 and ore >= c_geo_ore and obs >= c_geo_obs
       # Build geode robot
       stack << [r_ore, r_cly, r_obs, r_geo+1,
                 new_ore-c_geo_ore, new_cly, new_obs-c_geo_obs, new_geo,
-                time-1]
-      next # If we can build a geode robot, no need to try anything else
+                MASK_ALL, new_time]
+      new_mask &= ~MASK_GEO
     end
-
-    nothing_to_build = true
-    if r_obs < c_geo_obs and ore >= c_obs_ore and cly >= c_obs_cly
-      nothing_to_build = false
+    if mask & MASK_OBS != 0 and r_obs < c_geo_obs and
+        ore >= c_obs_ore and cly >= c_obs_cly
       # Build obsidian robot
       stack << [r_ore, r_cly, r_obs+1, r_geo,
                 new_ore-c_obs_ore, new_cly-c_obs_cly, new_obs, new_geo,
-                time-1]
+                MASK_ALL, new_time]
+      new_mask &= ~MASK_OBS
     end
-    if r_cly < c_obs_cly and ore >= c_cly_ore
-      nothing_to_build = false
+    if mask & MASK_CLY != 0 and r_cly < c_obs_cly and ore >= c_cly_ore
       # Build clay robot
       stack << [r_ore, r_cly+1, r_obs, r_geo,
                 new_ore-c_cly_ore, new_cly, new_obs, new_geo,
-                time-1]
+                MASK_ALL, new_time]
+      new_mask &= ~MASK_CLY
     end
-    if r_ore < max_c_ore and ore >= c_ore_ore
-      nothing_to_build = false
+    if mask & MASK_ORE != 0 and r_ore < max_c_ore and ore >= c_ore_ore
       # Build ore robot
       stack << [r_ore+1, r_cly, r_obs, r_geo,
                 new_ore-c_ore_ore, new_cly, new_obs, new_geo,
-                time-1]
+                MASK_ALL, new_time]
+      new_mask &= ~MASK_ORE
     end
 
-    if ore < max_c_ore or nothing_to_build
+    if ore < max_c_ore or new_mask == mask
       # If we don't have ores needed for all types (or can't build), try waiting
       # (Always build a robot if we have the max needed ores)
       stack << [r_ore, r_cly, r_obs, r_geo,
                 new_ore, new_cly, new_obs, new_geo,
-                time-1]
+                new_mask, new_time]
     end
   end
   return max_geo
