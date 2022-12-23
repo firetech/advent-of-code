56ef10278d47dfc048ccaec1fa86a36ceb62ce07
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2022-12-23 07:50:42 +0100

    Speed up 2022/23.
    With the usual trick of switching to bitmasked ints for storing
    positions, we save a bit more than half the original runtime.
    
    Furthermore, by using Hash instead of Set for elf storage, and having it
    do double duty as storage of where to put back elfs blocked by matching
    proposals, we save another few hundred milliseconds.

--- 2022/23/elfdiffusion.rb
+++ 2022/23/elfdiffusion.rb
@@ -1,18 +1,6 @@
-require 'set'
-
 file = ARGV[0] || 'input'
 #file = 'example1'
 #file = 'example2'
-
-map = File.read(file).rstrip.split("\n").map(&:chars)
-@elves = Set[]
-map.each_with_index do |line, y|
-  line.each_with_index do |char, x|
-    if char == '#'
-      @elves << [x, y]
-    end
-  end
-end
 
 NEIGHBOURS = [
   [-1, -1], [0, -1], [1, -1],
@@ -21,54 +9,86 @@ NEIGHBOURS = [
 ]
 
 DIRS = [
-  [[[-1, -1], [ 0, -1], [ 1, -1]], [ 0, -1]],
-  [[[-1,  1], [ 0,  1], [ 1,  1]], [ 0,  1]],
-  [[[-1, -1], [-1,  0], [-1,  1]], [-1,  0]],
-  [[[ 1, -1], [ 1,  0], [ 1,  1]], [ 1,  0]]
+  # [[Neighbours to check], [dx, dy]]
+  [[[-1, -1], [ 0, -1], [ 1, -1]], [ 0, -1]], # North
+  [[[-1,  1], [ 0,  1], [ 1,  1]], [ 0,  1]], # South
+  [[[-1, -1], [-1,  0], [-1,  1]], [-1,  0]], # West
+  [[[ 1, -1], [ 1,  0], [ 1,  1]], [ 1,  0]]  # East
 ]
+
+C_BITS = 8 # Has buffer, largest seen in my input was 120
+C_MASK = (1 << C_BITS) - 1
+NEG_MASK = 0b1
+def to_pos(x, y)
+  x_neg = (x < 0) ? 1 : 0
+  y_neg = (y < 0) ? 1 : 0
+  return y.abs << (C_BITS+2) | y_neg << (C_BITS+1) | x.abs << 1 | x_neg
+end
+def from_pos(pos)
+  x_neg = pos & NEG_MASK
+  pos >>= 1
+  x = pos & C_MASK
+  x = -x if x_neg == 1
+  pos >>= C_BITS
+  y_neg = pos & NEG_MASK
+  pos >>= 1
+  y = pos & C_MASK
+  y = -y if y_neg == 1
+  return x, y
+end
+
+map = File.read(file).rstrip.split("\n").map(&:chars)
+@elves = {}
+map.each_with_index do |line, y|
+  line.each_with_index do |char, x|
+    if char == '#'
+      @elves[to_pos(x, y)] = true
+    end
+  end
+end
 
 rounds = 0
 dir_offset = 0
-elves = @elves
 begin
   moved = 0
-  new_elves = Set[]
-  blocked = {}
-  @elves.each do |pos|
-    x, y = pos
-    unless NEIGHBOURS.any? { |dx, dy| @elves.include?([x+dx, y+dy]) }
-      # No neighbours, no need to move
-      new_elves << pos
-      next
-    end
+  new_elves = {}
+  blocked_proposals = []
+  @elves.each_key do |pos|
+    x, y = from_pos(pos)
     proposed = false
-    DIRS.length.times do |d|
-      checks, move = DIRS[(d+dir_offset) % DIRS.length]
-      unless checks.any? { |dx, dy| @elves.include?([x+dx, y+dy]) }
-        dx, dy = move
-        new_pos = [x+dx, y+dy]
-        check = blocked[new_pos]
-        proposed = true
-        if check.nil?
-          # No one else has proposed that move yet, try to move
-          blocked[new_pos] = pos
-          new_elves << new_pos
-          moved += 1
-        elsif check != false
-          # Move has been proposed by someone else, move them back
-          new_elves << blocked[new_pos]
-          new_elves.delete(new_pos)
-          new_elves << pos
-          blocked[new_pos] = false
-          moved -= 1
-        else
-          # Move has been proposed by >2 others, just don't move
-          new_elves << pos
+    if NEIGHBOURS.any? { |dx, dy| @elves.has_key?(to_pos(x+dx, y+dy)) }
+      # Neighbour found, try to move
+      DIRS.length.times do |d|
+        checks, move = DIRS[(d+dir_offset) % DIRS.length]
+        unless checks.any? { |dx, dy| @elves.has_key?(to_pos(x+dx, y+dy)) }
+          proposed = true
+          dx, dy = move
+          new_pos = to_pos(x+dx, y+dy)
+          other = new_elves[new_pos]
+          if other.nil?
+            # No one else has proposed that move yet, try to move
+            new_elves[new_pos] = pos
+            moved += 1
+          else
+            # Move has been proposed by someone else
+            if other != true
+              # Move original proposer back
+              new_elves[other] = true
+              new_elves[new_pos] = true
+              blocked_proposals << new_pos
+              moved -= 1
+            end
+            # Stay put
+            new_elves[pos] = true
+          end
+          break
         end
-        break
       end
     end
-    new_elves << pos unless proposed
+    new_elves[pos] = pos unless proposed
+  end
+  blocked_proposals.each do |pos|
+    new_elves.delete(pos)
   end
   @elves = new_elves
   dir_offset = (dir_offset + 1) % DIRS.length
@@ -78,7 +98,8 @@ begin
   if rounds == 10
     min_x = min_y = Float::INFINITY
     max_x = max_y = -Float::INFINITY
-    @elves.each do |x, y|
+    @elves.each_key do |pos|
+      x, y = from_pos(pos)
       min_x = x if x < min_x
       max_x = x if x > max_x
       min_y = y if y < min_y
