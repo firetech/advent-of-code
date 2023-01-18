0399ea00c6b96638d9962a9fca7d7ff9dd6fccb5
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-01-18 01:00:45 +0100

    Improve graphical output of 2016/08.
    Use \u2588 instead of #.

--- 2016/08/display.rb
+++ 2016/08/display.rb
@@ -11,7 +11,7 @@ File.read(file).strip.split("\n").each do |line|
   when /\Arect (\d+)x(\d+)\z/
     w, h = Regexp.last_match(1).to_i, Regexp.last_match(2).to_i
     h.times do |y|
-      grid[y][0...w] = ['#'] * w
+      grid[y][0...w] = ["\u2588"] * w
     end
   when /\Arotate row y=(\d+) by (\d+)\z/
     y, r = Regexp.last_match(1).to_i, Regexp.last_match(2).to_i
@@ -27,7 +27,7 @@ File.read(file).strip.split("\n").each do |line|
 end
 
 # Part 1
-puts "Lit pixels: #{grid.flatten.count('#')}"
+puts "Lit pixels: #{grid.flatten.count("\u2588")}"
 
 # Part 2
 puts grid.map(&:join).join("\n")
