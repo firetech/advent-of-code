d12274a248ffa8b28c9fc4cd9dadb8a9fe129285
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-12-05 14:57:27 +0100

    Optimized 2023/05.
    Now runs in mere milliseconds.

--- 2023/05/seeding.rb
+++ 2023/05/seeding.rb
@@ -3,9 +3,14 @@ require_relative '../../lib/aoc'
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
-@seed_line, *maps = File.read(file).rstrip.split("\n\n")
+seed_line, *maps = File.read(file).rstrip.split("\n\n")
+if seed_line =~ /\Aseeds: ((?:\d+(?:\s+|\z))+)/
+  @seeds = Regexp.last_match(1).split(/\s+/).map(&:to_i)
+else
+  raise "Malformed seed line: '#{@seed_line}'"
+end
 
-@maps = {}
+@maps = []
 maps.each do |map|
   header, *lines = map.split("\n")
   if header =~ /\A(.+)-to-(.+) map:\z/
@@ -14,7 +19,7 @@ maps.each do |map|
     raise "Malformed map header: '#{header}'"
   end
   map = []
-  @maps[id] = map
+  @maps << map
   lines.each do |line|
     case line
     when /\A(\d+) (\d+) (\d+)\z/
@@ -23,7 +28,6 @@ maps.each do |map|
       len = Regexp.last_match(3).to_i
       map << [
         (src...src+len),
-        (dst...dst+len),
         dst-src
       ]
     else
@@ -33,16 +37,11 @@ maps.each do |map|
 end
 
 # Part 1
-if @seed_line =~ /\Aseeds: ((?:\d+(?:\s+|\z))+)/
-  seeds = Regexp.last_match(1).split(/\s+/).map(&:to_i)
-else
-  raise "Malformed seed line: '#{@seed_line}'"
-end
 results = []
-seeds.each do |val|
-  @maps.each do |id, map|
-    map.each do |in_range, out_range, offset|
-      if in_range.include?(val)
+@seeds.each do |val|
+  @maps.each do |map|
+    map.each do |range, offset|
+      if range.include?(val)
         val += offset
         break
       end
@@ -53,31 +52,35 @@ end
 puts "Lowest location number (listed seeds): #{results.min}"
 
 # Part 2
-if @seed_line =~ /\Aseeds: ((?:\d+\s+\d+(?:\s+|\z))+)/
-  seeds2 = Regexp.last_match(1).split(/\s+/).map(&:to_i).each_slice(2).map do |src, len|
-    (src...src+len)
-  end
-else
-  raise "Malformed seed line: '#{@seed_line}'"
+@seed_ranges = @seeds.each_slice(2).map do |src, len|
+  src...(src+len)
 end
-loc = 0
-found = false
-until found
-  val = loc
-  @maps.reverse_each do |id, map|
-    map.each do |in_range, out_range, offset|
-      if out_range.include?(val)
-        val -= offset
-        break
+
+# Map to possible ranges through each conversion.
+output_ranges = @maps.inject(@seed_ranges) do |ranges, map|
+  ranges.flat_map do |src|
+    # Find intersections with map ranges.
+    intersections = map.filter_map do |m_range, m_offset|
+      next if src.max < m_range.min or m_range.max < src.min
+      int = [src.min, m_range.min].max..[src.max, m_range.max].min
+      [int, (int.min + m_offset)..(int.max + m_offset)]
+    end
+
+    # Generate list of possible output ranges from the input ranges.
+    dst = []
+    min = src.min
+    intersections.sort_by { |f, t| f.min }.each do |from, to|
+      if min < from.min
+        # Input outside mapping range, keep that part of the range as is.
+        dst << (min...from.min)
       end
+      dst << to
+      min = from.max + 1
     end
+    # Input after last mapping range
+    dst << (min..src.max) if src.max > min
+    dst
   end
-  seeds2.each do |range|
-    if range.include?(val)
-      puts "Lowest location number (seed ranges): #{loc}"
-      found = true
-      break
-    end
-  end
-  loc += 1
 end
+min_output = output_ranges.min_by(&:min).min
+puts "Lowest location number (seed ranges): #{min_output}"
