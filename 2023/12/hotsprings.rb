829db08fef56c4cd7b94c33d520d3bebdf7cf406
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-12-12 16:41:04 +0100

    Speed up 2023/12.
    * Add Multicore lib for parallelized processing of each input line.
    * Using the raw pattern strings instead of char arrays (which, in
      practice, are string arrays in Ruby) dropped the runtime by ~half
      after the Multicore conversion (probably due to simpler serialization
      for communication between main process and forks).

--- 2023/12/hotsprings.rb
+++ 2023/12/hotsprings.rb
@@ -1,29 +1,8 @@
 require_relative '../../lib/aoc'
+require_relative '../../lib/multicore'
 
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
-
-@list = []
-File.read(file).rstrip.split("\n").each do |line|
-  case line
-  when /\A([.?#]+) ((?:\d+(?:,|\z))+)/
-    @list << [
-      Regexp.last_match(1).chars.map do |c|
-        case c
-        when '.'
-          false
-        when '?'
-          nil
-        when '#'
-          true
-        end
-      end,
-      Regexp.last_match(2).split(',').map(&:to_i)
-    ]
-  else
-    raise "Malformed line: '#{line}'"
-  end
-end
 
 @cache = {}
 def count_possibles(pattern, groups, in_group = 0)
@@ -48,46 +27,63 @@ def inner_count_possibles(pattern, groups, in_group = 0)
   # Current group is too long
   return 0 if in_group > 0 and (groups.empty? or in_group > groups.first)
 
-  case pattern.first
-  when false
+  # Check first character and recurse to the next
+  remaining = pattern[1..-1]
+  case pattern[0]
+  when '.'
     if in_group > 0
-      if in_group != groups.first
-        return 0
-      end
+      # End current group, stop if it's the wrong length
+      return 0 if in_group != groups.first
       groups = groups[1..-1]
     end
-    return count_possibles(pattern[1..-1], groups, 0)
-  when true
-    return count_possibles(pattern[1..-1], groups, in_group+1)
-  when nil
+    return count_possibles(remaining, groups, 0)
+  when '#'
+    # Continue (or start) current group
+    return count_possibles(remaining, groups, in_group+1)
+  when '?'
     if groups.empty? or in_group == groups.first
       # No groups left, or current group is done, spring must be broken
-      groups = groups[1..-1] || []
-      return count_possibles(pattern[1..-1], groups, 0)
+      return count_possibles(remaining, groups[1..-1] || [], 0)
+    elsif in_group > 0
+      # Current group isn't done yet, spring must be operational
+      return count_possibles(remaining, groups, in_group+1)
     end
-    if in_group > 0
-      # Current group isn't filled yet, spring must be operational
-      return count_possibles(pattern[1..-1], groups, in_group+1)
-    end
-    # Group hasn't started, we'll have to try both alternatives
-    return count_possibles(pattern[1..-1], groups, in_group+1) +
-           count_possibles(pattern[1..-1], groups, in_group)
+    # Group hasn't started, we'll have to try both starting it and waiting
+    return count_possibles(remaining, groups, in_group+1) +
+           count_possibles(remaining, groups, in_group)
+  else
+    raise "Unexpected state: #{pattern[0]}"
   end
 end
 
-# Part 1
-sum = 0
-@list.each do |pattern, groups|
-  sum += count_possibles(pattern, groups)
+input, output, stop = Multicore.run do |worker_in, worker_out|
+  until (line = worker_in[]).nil?
+    case line
+    when /\A([.?#]+) ((?:\d+(?:,|\z))+)/
+      pattern = Regexp.last_match(1)
+      groups = Regexp.last_match(2).split(',').map(&:to_i)
+    else
+      raise "Malformed line: '#{line}'"
+    end
+
+    worker_out[[0, count_possibles(pattern, groups)]]
+    worker_out[[1, count_possibles(([pattern] * 5).join('?'), groups * 5)]]
+  end
 end
-puts "Sum of possible arrangement counts: #{sum}"
+lines = 0
+File.read(file).rstrip.split("\n").each do |line|
+  input << line
+  lines += 1
+end
+input.close
+sum = [0, 0]
+(lines * 2).times do
+  part, possibles = output.pop
+  sum[part] += possibles
+end
+
+# Part 1
+puts "Sum of possible arrangement counts: #{sum[0]}"
 
 # Part 2
-sum = 0
-@list.each do |pattern, groups|
-  sum += count_possibles(
-    pattern + ([nil] + pattern) * 4,
-    groups * 5
-  )
-end
-puts "Sum of possible arrangement counts (unfolded): #{sum}"
+puts "Sum of possible arrangement counts (unfolded): #{sum[1]}"
