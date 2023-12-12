7f82d9cbeb23faaf69e5c038a047320f538b3388
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2023-12-12 09:47:29 +0100

    2023/12 completed.

--- 2023/12/hotsprings.rb
+++ 2023/12/hotsprings.rb
@@ -25,60 +25,69 @@ File.read(file).rstrip.split("\n").each do |line|
   end
 end
 
-def filter_possible(list, groups)
-  return list.select do |pattern|
-    group = 0
-    in_group = 0
-    possible = true
-    done = true
-    (pattern + [false]).each do |val|
-      case val
-      when nil
-        # Can't know anything from this part on
-        done = false
-        break
-      when true
-        if group >= groups.length or in_group > groups[group]
-          possible = false
-          done = false
-          break
-        end
-        in_group += 1
-      when false
-        next if in_group == 0
-        if in_group == groups[group]
-          in_group = 0
-          group += 1
-        else
-          possible = false
-          done = false
-          break
-        end
+@cache = {}
+def count_possibles(pattern, groups, in_group = 0)
+  cache_key = [pattern, groups, in_group].hash
+  cache = @cache[cache_key]
+  if cache.nil?
+    cache = inner_count_possibles(pattern, groups, in_group)
+    @cache[cache_key] = cache
+  end
+  return cache
+end
+def inner_count_possibles(pattern, groups, in_group = 0)
+  # Base cases
+  if pattern.empty?
+    if in_group > 0
+      return (groups.length == 1 and in_group == groups.first) ? 1 : 0
+    else
+      return groups.empty? ? 1 : 0
+    end
+  end
+
+  # Current group is too long
+  return 0 if in_group > 0 and (groups.empty? or in_group > groups.first)
+
+  case pattern.first
+  when false
+    if in_group > 0
+      if in_group != groups.first
+        return 0
       end
+      groups = groups[1..-1]
     end
-    if done and group < groups.length
-      possible = false
+    return count_possibles(pattern[1..-1], groups, 0)
+  when true
+    return count_possibles(pattern[1..-1], groups, in_group+1)
+  when nil
+    if groups.empty? or in_group == groups.first
+      # No groups left, or current group is done, spring must be broken
+      groups = groups[1..-1] || []
+      return count_possibles(pattern[1..-1], groups, 0)
     end
-    possible
+    if in_group > 0
+      # Current group isn't filled yet, spring must be operational
+      return count_possibles(pattern[1..-1], groups, in_group+1)
+    end
+    # Group hasn't started, we'll have to try both alternatives
+    return count_possibles(pattern[1..-1], groups, in_group+1) +
+           count_possibles(pattern[1..-1], groups, in_group)
   end
 end
 
+# Part 1
 sum = 0
 @list.each do |pattern, groups|
-  possible = [pattern]
-  pattern.each_with_index do |val, i|
-    next unless val.nil?
-    new_possible = []
-    possible.each do |p|
-      p_false = p.clone
-      p_false[i] = false
-      new_possible << p_false
-      p_true = p.clone
-      p_true[i] = true
-      new_possible << p_true
-    end
-    possible = filter_possible(new_possible, groups)
-  end
-  sum += possible.length
+  sum += count_possibles(pattern, groups)
 end
 puts "Sum of possible arrangement counts: #{sum}"
+
+# Part 2
+sum = 0
+@list.each do |pattern, groups|
+  sum += count_possibles(
+    pattern + ([nil] + pattern) * 4,
+    groups * 5
+  )
+end
+puts "Sum of possible arrangement counts (unfolded): #{sum}"
