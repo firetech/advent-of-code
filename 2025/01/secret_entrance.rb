d8f5371e3f876594d74f9a28ac23ab254d997493
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-01 06:56:52 +0100

    Minor cleanup of 2025/01.

--- 2025/01/secret_entrance.rb
+++ 2025/01/secret_entrance.rb
@@ -4,24 +4,23 @@ file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 #file = 'example2'
 
-@dial = 50
+dial = 50
 @zeroes = 0  # Part 1
 @passes = 0  # Part 2
 File.read(file).rstrip.split("\n").each do |line|
   case line
   when /\A(L|R)(\d+)\z/
-    pos = Regexp.last_match(1) == 'R'
     move = Regexp.last_match(2).to_i
     @passes += move / 100
-    if pos
-      new_dial = (@dial + move) % 100
-      @passes += 1 if new_dial < @dial
+    if Regexp.last_match(1) == 'R'
+      new_dial = (dial + move) % 100
+      @passes += 1 if new_dial < dial  # Part 2
     else
-      new_dial = (@dial - move) % 100
-      @passes += 1 if (new_dial == 0 or new_dial > @dial) and @dial != 0
+      new_dial = (dial - move) % 100
+      @passes += 1 if (new_dial == 0 or new_dial > dial) and dial != 0  # Part 2
     end
-    @dial = new_dial
-    @zeroes += 1 if @dial == 0  # Part 1
+    dial = new_dial
+    @zeroes += 1 if dial == 0  # Part 1
   else
     raise "Malformed line: '#{line}'"
   end
