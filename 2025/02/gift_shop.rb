b020041bb3e2563d16d7d616c89f228f95ae2f94
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-02 06:58:14 +0100

    Slight optimization of 2025/02.
    Save time by only doing one Regexp (and one string conversion) per
    number to check.

--- 2025/02/gift_shop.rb
+++ 2025/02/gift_shop.rb
@@ -8,11 +8,13 @@ file = ARGV[0] || AOC.input_file()
 File.read(file).rstrip.split(",").each do |range|
   case range
   when /\A(\d+)-(\d+)\z/
-    min = Regexp.last_match(1).to_i
-    max = Regexp.last_match(2).to_i
-    min.upto(max) do |n|
-      @repeated_once += n if n.to_s =~ /\A(\d+)\1\z/  # Part 1
-      @repeated_mult += n if n.to_s =~ /\A(\d+)\1+\z/  # Part 2
+    Regexp.last_match(1).to_i.upto(Regexp.last_match(2).to_i) do |n|
+      if n.to_s =~ /\A(\d+)(\1+)\z/
+        if Regexp.last_match(1).length == Regexp.last_match(2).length
+          @repeated_once += n  # Part 1
+        end
+        @repeated_mult += n  # Part 2
+      end
     end
   else
     raise "Malformed range: '#{range}'"
