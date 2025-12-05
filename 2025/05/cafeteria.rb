c14d2f6488304871eca30da1e4ec21648645ca36
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-05 06:40:25 +0100

    Cleanup and minor optimization of 2025/05.
    * Change range data type from Array to Hash (with :min and :max keys),
      makes the intent clearer.
    * Remove pre-filtering of ranges in part 2, can easily be done in the
      main loop. Reduced the runtime by ~25%.

--- 2025/05/cafeteria.rb
+++ 2025/05/cafeteria.rb
@@ -9,7 +9,10 @@ ranges, ingredients = File.read(file).rstrip.split("\n\n")
 ranges.split("\n").each do |line|
   case line
   when /\A(\d+)-(\d+)\z/
-    @ranges << [Regexp.last_match(1).to_i, Regexp.last_match(2).to_i]
+    @ranges << {
+      min: Regexp.last_match(1).to_i,
+      max: Regexp.last_match(2).to_i
+    }
   else
     raise "Malformed line: '#{line}'"
   end
@@ -20,7 +23,7 @@ fresh = 0
 ingredients.split("\n").each do |line|
   case line
   when /\A\d+\z/
-    fresh += 1 if @ranges.any? { |r| line.to_i.between?(r.first, r.last) }
+    fresh += 1 if @ranges.any? { |r| line.to_i.between?(r[:min], r[:max]) }
   else
     raise "Malformed line: '#{line}'"
   end
@@ -28,21 +31,16 @@ end
 puts "Fresh ingredients: #{fresh}"
 
 # Part 2
-# Filter out ranges completely covered by another
-good_ranges = @ranges.uniq
-good_ranges = good_ranges.filter { |r| not good_ranges.any? { |rr| rr != r and rr.first <= r.first and rr.last >= r.last }}.sort_by(&:first)
-
-# Merge the rest
-@merged_ranges = [good_ranges.shift]
-last_range = @merged_ranges.first
-good_ranges.each do |r|
-  if last_range.last >= r.first
-    last_range[1] = r.last
+@merged_ranges = []
+last_range = nil
+@ranges.sort_by { |r| r[:min] }.each do |r|
+  if not last_range.nil? and last_range[:max] >= r[:min]
+    last_range[:max] = [last_range[:max], r[:max]].max
   else
     @merged_ranges << r
     last_range = r
   end
 end
-merged_total = @merged_ranges.sum { |r| r.last - r.first + 1 }
+merged_total = @merged_ranges.sum { |r| r[:max] - r[:min] + 1 }
 puts "Total possible fresh ingredients: #{merged_total}"
 
