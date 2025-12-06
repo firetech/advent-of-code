faa99351d16c599e8bfc4817e764ee6ad48747a1
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-06 06:26:11 +0100

    2025/06 completed.
    Array.transpose() is the hero of the day.

--- 2025/06/trash_compactor.rb
+++ 2025/06/trash_compactor.rb
@@ -3,12 +3,36 @@ require_relative '../../lib/aoc'
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
-@problems = File.read(file).rstrip.split("\n").map { |line| line.strip.split(/\s+/) }.transpose
+@columns = []
+curr_col = nil
+add_curr = ->() do
+  op = curr_col.pop.join.strip.to_sym
+  @columns << [curr_col, op]
+  curr_col = nil
+end
+File.read(file).split("\n").map(&:chars).transpose.each do |col|
+  if col.all?(" ")
+    add_curr[]
+  elsif curr_col.nil?
+    curr_col = col.map { |r| [r] }
+  else
+    curr_col.zip(col) do |curr, new|
+      curr << new
+    end
+  end
+end
+add_curr[]
 
-sum =  @problems.sum(0) do |problem|
-  op = problem.last.to_sym
-  numbers = problem[0..-2].map(&:to_i)
-  numbers.inject(&op)
+# Part 1
+sum1 = @columns.sum do |numbers, op|
+  numbers.map(&:join).map(&:to_i).inject(&op)
 end
 
-puts "Sum of problems: #{sum}"
+puts "Sum of problems: #{sum1}"
+
+# Part 2
+sum2 = @columns.sum do |numbers, op|
+  numbers.transpose.map(&:join).map(&:to_i).inject(&op)
+end
+
+puts "Sum of problems (right-to-left columns): #{sum2}"
