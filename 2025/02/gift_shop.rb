aa8388c7fc47b45fd7d5ab702cf14bca9162ce01
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-07 20:24:57 +0100

    More optimization of 2025/02.
    Calculate the sum of invalid IDs using simple math, without actually
    generating any invalid ID.
    
    This is now fast enough that Multicore is slowing it down, and, without
    it, it still runs up to 10 times faster than the previous solution (at
    least on a 5950X).
    
    Math inspired by https://github.com/jimm89/AdventOfCode2025/blob/main/Day%202/Day%202.ipynb

--- 2025/02/gift_shop.rb
+++ 2025/02/gift_shop.rb
@@ -1,7 +1,4 @@
-require 'set'
-
 require_relative '../../lib/aoc'
-require_relative '../../lib/multicore'
 
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
@@ -16,52 +13,110 @@ File.read(file).rstrip.split(",").each do |range|
   end
 end
 
+# Standard formula for sum of all numbers 1 to n (inclusive).
+def sum_first_n(n)
+  return n * (n + 1) / 2
+end
+
+# Sum of all numbers from min to max (inclusive).
+def sum_range(min, max)
+  return sum_first_n(max) - sum_first_n(min - 1)
+end
+
+# Sum of all numbers with repeated patterns from min_s (as a string) to max_s
+# (as a string), with the total number length of num_digits and a repeated
+# pattern length of pat_len.
+def sum_rep(min_s, max_s, num_digits, pat_len)
+  reps, rest = num_digits.divmod(pat_len)
+  return 0 if rest != 0  # num_digits can't be evenly split by pat_len.
+
+  min = min_s.to_i
+  max = max_s.to_i
+
+  pat_pow = 10**pat_len  # E.g. pat_len=2 -> 100
+  pat_start = pat_pow / 10  # E.g. pat_len=2 -> 10
+  pat_end = pat_pow - 1  # E.g. pat_len=2 -> 99
+
+  # E.g. pat_len=3, reps=3 -> 10000 (123456 / 10000 = 12)
+  pat_div = pat_pow ** (reps - 1)
+  # E.g. pat_len=2, reps=3 -> 10101 (12 * 10101 = 121212)
+  pat_mul = (pat_div*pat_pow - 1) / (pat_pow - 1)
+  #       == (10**(pat_len*reps) - 1) / (10**pat_len - 1)
+  # E.g.  == 999999 / 99 == 10101 (for pat_len=2, reps=3)
+
+  # Get the smallest repeatable pattern that fits in the range.
+  if num_digits > min_s.length
+    # num_digits is above the range's minimum number of digits, so the smallest
+    # number with the given pattern length (i.e. pat_start) will fit.
+    pat_min = pat_start
+  else
+    # Calculate the smallest repeatable pattern.
+    # E.g:
+    # - min=121212, pat_len=2, reps=3 -> 12 (since 121212 >= 121212)
+    # - min=121213, pat_len=2, reps=3 -> 13 (since 121212 < 121213)
+    pat_min = min / pat_div
+    pat_min += 1 if (pat_min * pat_mul) < min
+  end
+
+  # Get the largest repeateable pattern that fits in the range.
+  if num_digits < max_s.length
+    # num_digits is below the range's maximum number of digits, so the largest
+    # number with the given pattern length (i.e. pat_end) will fit.
+    pat_max = pat_end
+  else
+    # Calculate the largest repeatable pattern.
+    # E.g:
+    # - max=121212, pat_len=2, reps=3 -> 12 (since 121212 <= 121212)
+    # - max=121211, pat_len=2, reps=3 -> 11 (since 121212 > 121211)
+    pat_max = max / pat_div
+    pat_max -= 1 if (pat_max * pat_mul) > max
+  end
+
+  return 0 if pat_min > pat_max  # No possible numbers are in range.
+  return pat_min * pat_mul if pat_min == pat_max  # Only one possible number.
+
+  # Get the sum of all the possible patterns of the given length. This sum
+  # multiplied by pat_mul calculated above gives the sum of all the numbers that
+  # these patterns create.
+  return sum_range(pat_min, pat_max) * pat_mul
+end
+
 @repeated_once = 0  # Part 1
 @repeated_mult = 0  # Part 2
-stop = nil
-begin
-  max_threads = [8, @ranges.length].min
-  input, output, stop, nrunners = Multicore.run(-max_threads) do |worker_in, worker_out|
-    rep_once = 0  # Part 1
-    rep_mult = 0  # Part 2
-    loop do
-      data = worker_in[]
-      break if data == :done
+@ranges.each do |min_s, max_s|
+  # Loop through all possible number lengths that fit in this range.
+  min_s.length.upto(max_s.length) do |num_digits|
+    pat_sums = {}
+    sum = 0
+    max_pat_len = num_digits / 2
+    # Loop through all possible pattern lengths that fit in num_digits.
+    1.upto(max_pat_len) do |pat_len|
+      # Get and save the sum of all pattern numbers with this number of digits
+      # and this pattern length
+      pat_sum = sum_rep(min_s, max_s, num_digits, pat_len)
+      pat_sums[pat_len] = pat_sum
 
-      min, max = data
-      range = (min.to_i..max.to_i)
-
-      # Generate all the invalid numbers in range
-      min.length.upto(max.length) do |num_digits|
-        invalids_once = Set[]
-        invalids_mult = Set[]
-        1.upto(num_digits/2).each do |pat_len|
-          reps, rest = num_digits.divmod(pat_len)
-          next if rest != 0  # Must be an even split
-          pat_start = 10**(pat_len - 1)  # e.g. pat_len=3 -> 100
-          pat_end = pat_start *  10 - 1  # e.g. pat_len=3 -> 999
-          (pat_start).upto(pat_end) do |pat|
-            num = (pat.to_s * reps).to_i
-            next unless range.include?(num)
-            invalids_once << num if reps == 2
-            invalids_mult << num
-          end
+      # If any pattern numbers could be created
+      if pat_sum > 0
+        sum += pat_sum  # Add this sum to our total...
+        1.upto(pat_len - 1) do |factor_len|
+          # ...and remove any sums with a pattern length that's a factor of this
+          # pattern length from the total, to avoid double counting numbers.
+          # (E.g. 111111 is a repeated patterns with pattern lengths 1, 2 and 3,
+          # and should thus be removed from the totals for lengths 2 and 3,
+          # while still including e.g. 121212 and 123123 in the total.)
+          sum -= pat_sums[factor_len] if pat_len % factor_len == 0
         end
-        rep_once += invalids_once.sum
-        rep_mult += invalids_mult.sum
       end
     end
-    worker_out[[rep_once, rep_mult]]
+    # Part 1 (add the sum of numbers with the max length if this splits the
+    # total length evenly).
+    if max_pat_len + max_pat_len == num_digits
+      @repeated_once += pat_sums[max_pat_len]
+    end
+    # Part 2 (add the total sum, minus potential double counts).
+    @repeated_mult += sum
   end
-  @ranges.each { |range| input << range }
-  nrunners.times do
-    input << :done
-    rep_once, rep_mult = output.pop
-    @repeated_once += rep_once
-    @repeated_mult += rep_mult
-  end
-ensure
-  stop[] unless stop.nil?
 end
 
 # Part 1
