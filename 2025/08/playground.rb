4d96fb60fca71ac6b58d018aa90ba43d82e53742
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-08 07:19:49 +0100

    Some optimization of 2025/08.
    * When using box coordinates as keys, apply the bitmask trick.
    * Put each box in its own circuit before starting to connect them,
      simplifies the connection logic.
    
    => Runs 2-3 times faster.

--- 2025/08/playground.rb
+++ 2025/08/playground.rb
@@ -3,46 +3,47 @@ require_relative '../../lib/aoc'
 file = ARGV[0] || AOC.input_file(); part1_connections = (ARGV[1] || 1000).to_i
 #file = 'example1'; part1_connections = 10
 
+@max_coord = 0
 @boxes = File.read(file).rstrip.split("\n").map do |line|
-  line.split(',').map(&:to_i)
+  coords = line.split(',').map(&:to_i)
+  max_coord = coords.max
+  @max_coord = max_coord if max_coord > @max_coord
+  coords
 end
 
-@connections = @boxes.combination(2).map do |box1, box2|
-  x1, y1, z1 = box1
-  x2, y2, z2 = box2
-  dist = Math.sqrt((x1-x2).abs**2 + (y1-y2).abs**2 + (z1-z2).abs**2)
-  [box1, box2, dist]
+COORD_BITS = Math.log2(@max_coord).floor + 1
+def to_box(coords)
+  x, y, z = coords
+  return (x << COORD_BITS | y ) << COORD_BITS | z
 end
 
-@connections.sort_by!(&:last)
+@connections = @boxes.combination(2).sort_by do |(x1, y1, z1), (x2, y2, z2)|
+  # Sorting by distance squared should give the same result as by distance.
+  # => We can skip sqrt().
+  (x1-x2).abs**2 + (y1-y2).abs**2 + (z1-z2).abs**2
+end
 
 @box_circuit = {}
 @circuits = {}
-@next_circ = 1
+@boxes.each_with_index do |coords, i|
+  box = to_box(coords)
+  @box_circuit[box] = i
+  @circuits[i] = [box]
+end
+
 @next_i = 0
 def connect
-  return if @next_i >= @connections.length
-  box1, box2, _ = @connections[@next_i]
+  raise "No more connections" if @next_i >= @connections.length
+  coords1, coords2 = @connections[@next_i]
   @next_i += 1
-  circ1 = @box_circuit[box1]
-  circ2 = @box_circuit[box2]
-  if circ1.nil? and circ2.nil?
-    @box_circuit[box1] = @next_circ
-    @box_circuit[box2] = @next_circ
-    @circuits[@next_circ] = [box1, box2]
-    @next_circ += 1
-  elsif circ1.nil?
-    @box_circuit[box1] = circ2
-    @circuits[circ2] << box1
-  elsif circ2.nil?
-    @box_circuit[box2] = circ1
-    @circuits[circ1] << box2
-  elsif circ1 != circ2
+  circ1 = @box_circuit[to_box(coords1)]
+  circ2 = @box_circuit[to_box(coords2)]
+  if circ1 != circ2
     c2_boxes = @circuits.delete(circ2)
     @circuits[circ1] += c2_boxes
     c2_boxes.each { |b| @box_circuit[b] = circ1 }
   end
-  return box1, box2
+  return coords1.first, coords2.first
 end
 
 # Part 1
@@ -51,7 +52,7 @@ largest3 = @circuits.values.map(&:length).sort.last(3)
 puts "Sizes of the largest 3 circuits: #{largest3.inject(:*)}"
 
 # Part 2
-while @circuits.count > 1 or @box_circuit.count < @boxes.length
-  box1, box2 = connect
+while @circuits.count > 1
+  x1, x2 = connect
 end
-puts "Product of last X coordinates: #{box1.first * box2.first}"
+puts "Product of last X coordinates: #{x1*x2}"
