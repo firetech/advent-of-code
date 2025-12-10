0fb1c0178b4b6250487f0a6fbf0191e4331471e4
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-10 07:20:58 +0100

    2025/10 completed.
    ...using Z3. We'll see if I get around to make a non-Z3 solution.

--- 2025/10/factory_z3.rb
+++ 2025/10/factory_z3.rb
@@ -1,5 +1,6 @@
 require 'set'
 require_relative '../../lib/aoc'
+require 'z3'
 
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
@@ -15,14 +16,15 @@ File.read(file).rstrip.split("\n").each do |line|
     buttons = Regexp.last_match(2).lstrip.split(/\s+/).map do |btn|
       case btn
       when /\A\(([0-9,]+)\)\z/
-        Regexp.last_match(1).split(',').map(&:to_i).inject(0) do |r, b|
-          r | (1 << b)
-        end
+        Regexp.last_match(1).split(',').map(&:to_i)
       else
         raise "Malformed button: '#{btn}'"
       end
     end
-    @machines << [pattern, buttons, joltage]
+    @machines << [
+      [pattern, buttons.map { |btn| btn.inject(0) { |r, b| r | (1 << b) } }],
+      [joltage, buttons.map(&:to_set)],
+    ]
   else
     raise "Malformed line: '#{line}'"
   end
@@ -30,7 +32,7 @@ end
 
 # Part 1
 @sum_presses1 = 0
-@machines.each do |pattern, buttons, _|
+@machines.each do |(pattern, buttons), _|
   visited = Set[0]
   queue = [[0, 0]]
   until queue.empty?
@@ -49,3 +51,23 @@ end
   end
 end
 puts "Minimum number of button presses for indicators: #{@sum_presses1}"
+
+# Part 2
+@sum_presses2 = 0
+@machines.each do |_, (joltage, buttons)|
+  solver = Z3::Optimize.new
+  presses = []
+  buttons.each_index do |i|
+    btn_presses = Z3::Int("button_#{i}")
+    solver.assert(btn_presses >= 0)
+    presses << btn_presses
+  end
+  joltage.each_with_index do |value, counter|
+    affecting_buttons = buttons.filter_map.with_index { |btn, b| b if btn.include?(counter) }
+    solver.assert(affecting_buttons.map { |b| presses[b] }.sum == value)
+  end
+  solver.minimize(presses.sum)
+  raise 'Unsatisfiable?!' unless solver.satisfiable?
+  @sum_presses2 += presses.map { |var| solver.model[var].to_i }.sum
+end
+puts "Minimum number of button presses for joltage: #{@sum_presses2}"
