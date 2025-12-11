ca90e52728f99333d7239b931959171776645623
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-11 06:20:18 +0100

    2025/11 completed.

--- 2025/11/reactor.rb
+++ 2025/11/reactor.rb
@@ -1,7 +1,9 @@
+require 'set'
 require_relative '../../lib/aoc'
 
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
+#file = 'example2'
 
 @connections = {}
 File.read(file).rstrip.split("\n").each do |line|
@@ -30,3 +32,25 @@ def num_routes_from(node, cache = {})
 end
 
 puts "Number or routes from 'you' to 'out': #{num_routes_from(:you)}"
+
+# Part 2
+def num_routes_from_via(node, passed_dac = false, passed_fft = false, cache = {})
+  return (passed_dac and passed_fft) ? 1 : 0 if node == :out
+  cache_key = [node, passed_dac, passed_fft].hash
+  routes = cache[cache_key]
+  if routes.nil?
+    routes = 0
+    @connections[node].each do |next_node|
+      routes += num_routes_from_via(
+        next_node,
+        (passed_dac or node == :dac),
+        (passed_fft or node == :fft),
+        cache
+      )
+    end
+    cache[cache_key] = routes
+  end
+  return routes
+end
+
+puts "Number or routes from 'svr' to 'out', via 'dac' and 'fft': #{num_routes_from_via(:svr)}"
