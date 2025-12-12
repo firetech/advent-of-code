18e3d53c3d7608e2ca4c55a1c76cdf09e04bc555
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-12 08:12:31 +0100

    Replace slow 2025/10 solution with a somewhat faster bifurcation solution.

--- 2025/10/factory_slow.rb
+++ 2025/10/factory_slow.rb
@@ -1,6 +1,5 @@
 require 'set'
 require_relative '../../lib/aoc'
-require_relative '../../lib/priority_queue'
 require_relative '../../lib/multicore'
 
 file = ARGV[0] || AOC.input_file()
@@ -23,112 +22,61 @@ File.read(file).rstrip.split("\n").each do |line|
       end
     end
     @machines << [
-      pattern, buttons.map { |btn| btn.inject(0) { |r, b| r | (1 << b) } },
-      joltage, buttons.map(&:to_set),
+      pattern,
+      joltage,
+      buttons.map { |btn| btn.inject(0) { |r, b| r | (1 << b) } },
+      buttons.map { |btn| joltage.each_index.map { |i| btn.include?(i) ? 1 : 0 } },
     ]
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-
 # For part 2
-class ButtonPresser
-  def initialize(joltage, buttons)
-    @joltage = joltage
-    @num_joltage = joltage.length
-
-    # Sort buttons.
-    # * Most unique counter first.
-    # * When multiple buttons affect the same counter, sort by decreasing amount
-    #   of affected counters.
-    @buttons = []
-    @num_buttons = buttons.length
-    buttons_left = buttons.to_set
-    until buttons_left.empty?
-      counts = Hash.new(0)
-      buttons_left.each do |affecting|
-        affecting.each { |i| counts[i] += 1 }
+def part2(joltage, coeff_buttons)
+  pattern_costs = {}
+  num_buttons = coeff_buttons.length
+  num_outputs = coeff_buttons[0].length
+  0.upto(num_buttons) do |pattern_length|
+    coeff_buttons.combination(pattern_length) do |included_buttons|
+      pattern = num_outputs.times.map do |i|
+        included_buttons.map { |b| b[i] }.sum
       end
-      min_affected = counts.key(counts.values.min)
-      next_btn = nil
-      buttons_left.each do |btn|
-        next unless btn.include?(min_affected)
-        next_btn = btn if next_btn.nil? or btn.count > next_btn.count
-      end
-      buttons_left.delete(next_btn)
-      @buttons << next_btn
-    end
-
-    # Calculate number of buttons available for each counter after considering
-    # each button (in the order above) done.
-    @buttons_remaining_per_counter = @buttons.map { @joltage.map { 0 } }
-    @buttons.each_index.reverse_each do |b|
-      @joltage.each_index do |id|
-        @buttons_remaining_per_counter[b][id] += (
-          @buttons_remaining_per_counter[b + 1][id] rescue 0
-        )
-      end
-      @buttons[b].each do |id|
-        @buttons_remaining_per_counter[b][id] += 1
-      end
-    end
-
-    @best = Float::INFINITY
-  end
-
-  def traverse(remaining = @joltage, button_index = 0, presses = 0)
-    return if presses >= @best or presses + remaining.max >= @best  # Can't beat
-
-    if button_index == @num_buttons
-      @best = presses if remaining.all?(0)
-      return
-    end
-
-    # Find boundaries for this button given the circumstances.
-    min = 0
-    max = Float::INFINITY
-    button = @buttons[button_index]
-    button.each do |id|
-      r = remaining[id]
-      max = r if r < max
-      if @buttons_remaining_per_counter[button_index][id] == 1
-        min = r if r > min
-      end
-    end
-
-    return if min > max
-
-    # Try all the possibilites and go on to the next button.
-    max.downto(min) do |new_presses|
-      new_remaining = remaining.map.with_index do |curr, id|
-        this_presses = (button.include?(id) ? new_presses : 0)
-        break if this_presses > curr
-        curr - this_presses
-      end
-      next if new_remaining.nil?
-      traverse(new_remaining, button_index + 1, presses + new_presses)
+      pattern_costs[pattern] ||= pattern_length
     end
   end
 
-  def solve
-    traverse
-    return @best
+  cache = {}
+  bifurcate = ->(target) do
+    return 0 if target.all?(0)
+    cache_key = target.hash
+    result = cache[cache_key]
+    if result.nil?
+      result = Float::INFINITY
+      pattern_costs.each do |pattern, cost|
+        next if pattern.zip(target).any? { |p, t| p > t or p % 2 != t % 2 }
+        new_target = pattern.zip(target).map { |p, t| (t - p) / 2 }
+        this_cost = 2 * bifurcate[new_target] + cost
+        result = this_cost if this_cost < result
+      end
+      cache[cache_key] = result
+    end
+    return result
   end
+
+  return bifurcate[joltage]
 end
+
 
 @sum_presses1 = 0  # Part 1
 @sum_presses2 = 0  # Part 2
 stop = nil
 begin
-  input, output, stop, nrunners = Multicore.run(-@machines.length) do |worker_in, worker_out|
+  max_runners = [16, @machines.length].min
+  input, output, stop, nrunners = Multicore.run(-max_runners) do |worker_in, worker_out|
     sum_presses1 = 0  # Part 1
     sum_presses2 = 0  # Part 2
-    loop do
-      machine = worker_in[]
-      break if machine == :done
-
-      pattern, buttons1, joltage, buttons2 = machine
+    worker_in[].each do |pattern, joltage, pattern_buttons, coeff_buttons|
       # Part 1
       visited = Set[0]
       queue = [[0, 0]]
@@ -141,20 +89,20 @@ begin
         end
 
         new_presses = presses + 1
-        buttons1.each do |btn|
+        pattern_buttons.each do |btn|
           new_state = state ^ btn
           queue << [new_state, new_presses] if visited.add?(new_state)
         end
       end
 
       # Part 2
-      sum_presses2 += ButtonPresser.new(joltage, buttons2).solve()
+      sum_presses2 += part2(joltage, coeff_buttons)
     end
     worker_out[[sum_presses1, sum_presses2]]
   end
-  @machines.each { |m| input << m }
-  nrunners.times do
-    input << :done
+  machines_slice = (@machines.length / nrunners.to_f).ceil
+  @machines.each_slice(machines_slice).with_index { |list| input << list }
+  (@machines.length / machines_slice.to_f).ceil.times do |i|
     runner_presses1, runner_presses2 = output.pop
     print '.'
     @sum_presses1 += runner_presses1
