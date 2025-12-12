994ae9233bca99091dc53009d799d31d9ce89c15
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-12 10:28:24 +0100

    More correct solution to 2025/12.
    Still not complete, and still not working for the example, but is more
    clear about _why_ it works for the input (there are no regions with less
    than 3x3 per present but still space for all segments of the presents).

--- 2025/12/christmas_tree_farm.rb
+++ 2025/12/christmas_tree_farm.rb
@@ -11,18 +11,30 @@ file = ARGV[0] || AOC.input_file()
 end
 
 # This doesn't work for the example, but works fine for the real input
-@working = 0
+@definitely = 0
+@possible = 0
+@impossible = 0
 regions.split("\n").each do |line|
   case line
   when /\A(\d+)x(\d+):((?:\s+\d+){6})\z/
     width = Regexp.last_match(1).to_i
     height = Regexp.last_match(2).to_i
     counts = Regexp.last_match(3).lstrip.split(/\s+/).map(&:to_i)
-    total = counts.zip(@presents).map { |count, size| count * size }.sum
-    @working += 1 if total <= width * height
+    num_presents = counts.sum.to_f
+    num_segments = counts.zip(@presents).map { |count, size| count * size }.sum
+    if (width / 3) * (height / 3) >= num_presents
+      @definitely += 1
+    elsif num_segments <= width * height
+      @possible += 1
+    else
+      @impossible += 1
+    end
   else
     raise "Malformed line: '#{line}'"
   end
 end
 
-puts "Regions fitting all listed presents: #{@working}"
+puts "Region counts:"
+puts "#{@definitely} regions with at least one 3x3 region per present"
+puts "#{@possible} regions with less than 3x3 per present, but all segments fitting"
+puts "#{@impossible} regions with less space than total segments"
