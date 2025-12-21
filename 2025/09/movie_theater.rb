dc28baec40f46e14a765232bbcf6e418406e6a03
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2025-12-21 01:23:29 +0100

    Speed up 2025/09.
    Reduce runtime by ~60% by:
    * Using the part 1 calculation to precalculate and sort all possible
      areas by size.
    * Precalculate all polygon lines, and sort them by length.
    * Use these lists of areas and lines, created before forking, instead of
      sending lots of data through Multicore IPC.
    
    Since the areas are sorted by size beforehand, each worker knows it can
    exit as soon as it found a good area for part 2.

--- 2025/09/movie_theater.rb
+++ 2025/09/movie_theater.rb
@@ -9,15 +9,41 @@ file = ARGV[0] || AOC.input_file()
 end
 
 # Part 1
-@largest_area1 = 0
-@tiles.combination(2) do |(x1, y1), (x2, y2)|
+@areas = @tiles.combination(2).map do |(x1, y1), (x2, y2)|
   area = ((x1 - x2).abs + 1) * ((y1 - y2).abs + 1)
-  @largest_area1 = area if area > @largest_area1
+  [area, x1, y1, x2, y2]
 end
-puts "Largest rectangle area: #{@largest_area1}"
-
+@areas.sort_by! { |a| -a.first }
+puts "Largest rectangle area: #{@areas.first.first}"
 
 # Part 2
+@lines = []
+px1, py1 = @tiles.last
+@tiles.each do |px2, py2|
+  if px1 <= px2
+    min_px = px1
+    max_px = px2
+  else
+    min_px = px2
+    max_px = px1
+  end
+  if py1 <= py2
+    min_py = py1
+    max_py = py2
+  else
+    min_py = py2
+    max_py = py1
+  end
+
+  # Vertical/horizontal lines only, length is easy
+  length = max_px - min_px + max_py - min_py
+  @lines << [length, min_px, max_px, min_py, max_py]
+
+  px1 = px2
+  py1 = py2
+end
+@lines.sort_by! { |l| -l.first }
+
 # Technically not correct, just checking if any edge of the polygon intersects
 # the rectangle. Turns out to be good enough for this problem.
 #
@@ -30,7 +56,7 @@ puts "Largest rectangle area: #{@largest_area1}"
 #     +------+
 # This polygon is available as "test1" in this directory. Answer should be 40
 # for both part 1 and 2, but this solution returns 20.
-def rectangle_in_polygon?(x1, y1, x2, y2, polygon)
+def rectangle_ok?(x1, y1, x2, y2)
   if x1 <= x2
     min_x = x1
     max_x = x2
@@ -46,36 +72,18 @@ def rectangle_in_polygon?(x1, y1, x2, y2, polygon)
     max_y = y1
   end
 
-  px1, py1 = polygon.last
-  polygon.each do |px2, py2|
-    if py1 == py2
-      if px1 <= px2
-        min_px = px1
-        max_px = px2
-      else
-        min_px = px2
-        max_px = px1
-      end
-      return false if min_y < py1 and py1 < max_y and
+  @lines.each do |_, min_px, max_px, min_py, max_py|
+    if min_py == max_py
+      return false if min_y < min_py and min_py < max_y and
           ((min_px <= min_x and min_x < max_px) or
             (min_px < max_x and max_x <= max_px))
-    elsif px1 == px2
-      if py1 <= py2
-        min_py = py1
-        max_py = py2
-      else
-        min_py = py2
-        max_py = py1
-      end
-      return false if min_x < px1 and px1 < max_x and
+    elsif min_px == max_px
+      return false if min_x < min_px and min_px < max_x and
           ((min_py <= min_y and min_y < max_py) or
             (min_py < max_y and max_y <= max_py))
     else
       raise "Polygon not rectilinear"
     end
-
-    px1 = px2
-    py1 = py2
   end
 
   return true
@@ -84,19 +92,19 @@ end
 @largest_area2 = 0
 stop = nil
 begin
-  n_combinations = (@tiles.length * (@tiles.length - 1)) / 2
-  input, output, stop, nrunners = Multicore.run(-n_combinations) do |worker_in, worker_out|
+  n_areas = @areas.length
+  _, output, stop, n_runners = Multicore.run(-n_areas) do |_, worker_out, runner, n_runners|
     largest_area = 0
-    worker_in[].each do |(x1, y1), (x2, y2)|
-      next unless rectangle_in_polygon?(x1, y1, x2, y2, @tiles)
-      area = ((x1 - x2).abs + 1) * ((y1 - y2).abs + 1)
-      largest_area = area if area > largest_area
+    runner.step(n_areas-1, n_runners) do |i|
+      area, x1, y1, x2, y2 = @areas[i]
+      if rectangle_ok?(x1, y1, x2, y2)
+        largest_area = area
+        break # Areas are already sorted by size
+      end
     end
     worker_out[largest_area]
   end
-  slice = (n_combinations / nrunners.to_f).ceil
-  @tiles.combination(2).each_slice(slice) { |list| input << list }
-  (n_combinations / slice.to_f).ceil.times do
+  n_runners.times do
     area = output.pop
     @largest_area2 = area if area > @largest_area2
   end
