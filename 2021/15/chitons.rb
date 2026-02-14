c98ba1614c76eedd1733e97ddda017dd7baffb16
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2026-02-14 18:52:20 +0100

    Optimize 2021/15 by switching from Complex coords to bitmasks.
    Runs about 55% faster.

--- 2021/15/chitons.rb
+++ 2021/15/chitons.rb
@@ -5,13 +5,38 @@ file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
 @map = File.read(file).strip.split("\n").map { |line| line.chars.map(&:to_i) }
+MAP_HEIGHT = @map.length
+
+# This works for negative coordinates, BUT ONLY if they're only ever used as
+# delta values.
+# I.e.
+#   to_pos(3, 4) + to_pos(-2, -3) == to_pos(1, 1)
+# but
+#   from_pos(to_pos(-2, -3)) != [-2, -3]
+Y_BITS = Math.log2(MAP_HEIGHT*5-1).floor + 1
+Y_MASK = (1 << Y_BITS) - 1
+def to_pos(x, y)
+  return (x << Y_BITS) + y
+end
+def from_pos(pos)
+  return pos >> Y_BITS, pos & Y_MASK
+end
+
+DIRS = [
+  to_pos(0, -1),
+  to_pos(0,  1),
+  to_pos(-1, 0),
+  to_pos( 1, 0),
+]
 
 def dijkstra(map)
   # Copied from 2018/22. ^_^
-  target = Complex(map.first.length - 1, map.length - 1)
-  x_range = (0..target.real)
-  y_range = (0..target.imag)
-  start = Complex(0, 0)
+  tx = map.first.length - 1
+  ty = map.length - 1
+  target = to_pos(tx, ty)
+  x_range = (0..tx)
+  y_range = (0..ty)
+  start = to_pos(0, 0)
   dist = Hash.new(Float::INFINITY)
   dist[start] = 0
   queue = PriorityQueue.new
@@ -24,10 +49,11 @@ def dijkstra(map)
     end
 
     this_dist = dist[pos]
-    [ -1i, 1i, -1, 1 ].each do |delta|
+    DIRS.each do |delta|
       npos = pos + delta
-      next unless x_range.include?(npos.real) and y_range.include?(npos.imag)
-      ndist = this_dist + map[npos.imag][npos.real]
+      nx, ny = from_pos(npos)
+      next unless x_range.include?(nx) and y_range.include?(ny)
+      ndist = this_dist + map[ny][nx]
       if ndist < dist[npos]
         dist[npos] = ndist
         queue.push(npos, ndist)
